c3b0563cb4a1afacc0178cfb227c2a5c3186dd03
Jason Swett <jason@benfranklinlabs.com>
2024-10-26 12:51:31 -0400

    Make it so finished scope goes off of exit code. (#13)

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -5,11 +5,11 @@ FactoryBot.define do
     job_machine_id { SecureRandom.hex(6) }
 
     trait :passed do
-      test_output { "Script done on 2024-10-20 13:41:25+00:00 [COMMAND_EXIT_CODE=\"0\"]" }
+      exit_code { 0 }
     end
 
     trait :failed do
-      test_output { "Script done on 2024-10-20 13:41:25+00:00 [COMMAND_EXIT_CODE=\"1\"]" }
+      exit_code { 1 }
     end
   end
 end
--- spec/jobs/job_status/models/job_status_spec.rb
+++ spec/jobs/job_status/models/job_status_spec.rb
@@ -1,34 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Job, type: :model do
-  let!(:job) { create(:job) }
-
   describe "#status" do
-    context "there's no report yet" do
-      it "returns 'Running'" do
-        expect(job.status).to eq("Running")
-      end
-    end
-
     context "exit code is nil" do
       it "returns 'Running'" do
-        allow(job).to receive(:exit_code).and_return(nil)
+        job = create(:job)
         expect(job.status).to eq("Running")
       end
     end
 
     context "exit code is 0" do
       it "returns 'Passed'" do
-        allow(job).to receive(:exit_code).and_return(0)
-        job.job_events.create!(type: "job_finished")
+        job = create(:job, exit_code: 0)
         expect(job.status).to eq("Passed")
       end
     end
 
     context "exit code is not 0" do
       it "returns 'Failed'" do
-        allow(job).to receive(:exit_code).and_return(1)
-        job.job_events.create!(type: "job_finished")
+        job = create(:job, exit_code: 1)
         expect(job.status).to eq("Failed")
       end
     end
