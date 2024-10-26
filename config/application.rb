8102e2c673a60596ed2d48b611db180aaba8d40f
Jason Swett <jason@benfranklinlabs.com>
2024-10-26 11:48:12 -0400

    Fix to_time warning.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Saturn
     # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join("lib")
 
+    config.active_support.to_time_preserves_timezone = :zone
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
