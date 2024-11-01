b34cd280ecc8e67592a3630df1e37938341e1bcc
Jason Swett <jason@benfranklinlabs.com>
2024-11-01 16:38:25 -0400

    Add auth to admin.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_27_201813) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_01_202138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -113,6 +113,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_27_201813) do
     t.string "name"
     t.string "provider"
     t.string "uid"
+    t.boolean "super_admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
