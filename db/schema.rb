6e4140229dda454f4d8e634aed8c1685c1f77012
Jason Swett <jason@benfranklinlabs.com>
2024-11-01 16:41:52 -0400

    Add GitHubEvent.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_01_202138) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_01_204057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_01_202138) do
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_charges_on_job_id"
     t.index ["job_id"], name: "unique_index_on_charges_job_id", unique: true
+  end
+
+  create_table "github_events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.jsonb "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "job_events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
