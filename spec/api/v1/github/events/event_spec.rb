6fa5ad1cd7002cabecef402bb9347ce84bd5873a
Jason Swett <jason@benfranklinlabs.com>
2024-11-01 21:24:30 -0400

    Add test.

--- spec/api/v1/github/events/event_spec.rb
+++ spec/api/v1/github/events/event_spec.rb
@@ -2,6 +2,37 @@ require "rails_helper"
 include APIAuthenticationHelper
 
 RSpec.describe "GitHub Events", type: :request do
+  describe "installation event" do
+    before do
+      user = create(:user, uid: "55555", provider: "github")
+    end
+
+    let!(:payload) do
+      {
+        "action" => "created",
+        "installation" => {
+          "id" => "12345",
+          "account" => {
+            "type" => "Organization",
+          }
+        },
+        "sender" => {
+          "id" => "55555",
+        }
+      }.to_json
+    end
+
+    it "creates an installation" do
+      expect {
+        post(
+          "/api/v1/github_events",
+          params: payload,
+          headers: api_authorization_headers.merge('CONTENT_TYPE' => 'application/json')
+        )
+      }.to change { SaturnInstallation.count }.by(1)
+    end
+  end
+
   describe "git push event" do
     let!(:payload) do
       {
