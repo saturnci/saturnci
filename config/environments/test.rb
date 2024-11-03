c689b16ef6686d6f099ca2ba80d275a5a46d51ae
Jason Swett <jason@benfranklinlabs.com>
2024-11-03 10:39:51 -0500

    Fix missing encryption key issue.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -52,11 +52,9 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+  config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+  config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
 
   config.hosts << "www.example.com"
   config.hosts << IPAddr.new("0.0.0.0/0")
