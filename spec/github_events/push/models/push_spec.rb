3716a0ae9c937a1cb3b56ee586841992970da540
Jason Swett <jason@benfranklinlabs.com>
2024-11-06 14:06:23 -0500

    Add push stuff.

---
+++ spec/github_events/push/models/push_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "push" do
+  let!(:payload) do
+    {
+      "ref": "refs/heads/main",
+      "repository": {
+        "id": 123,
+        "name": "test",
+        "full_name": "user/test",
+      },
+      "pusher": {
+        "name": "user",
+      },
+      "head_commit": {
+        "id": "abc123",
+        "message": "commit message",
+        "author": {
+          "name": "author name",
+          "email": "author email",
+        },
+      },
+    }.with_indifferent_access
+  end
+
+  let!(:push_event) do
+    GitHubEvents::Push.new(payload, "user/test")
+  end
+
+  context "start builds automatically" do
+    let!(:project) { create(:project, start_builds_automatically_on_git_push: true) }
+
+    it "starts the build" do
+      build = Build.new(project:)
+      expect(build).to receive(:start!)
+      push_event.prepare_build(build)
+    end
+  end
+
+  context "do not start builds automatically" do
+    let!(:project) { create(:project, start_builds_automatically_on_git_push: false) }
+
+    it "does not start the build" do
+      build = Build.new(project:)
+      expect(build).not_to receive(:start!)
+      push_event.prepare_build(build)
+    end
+  end
+end
