737fa1ff74a8c7d7ed30bcd957901e3fdfd7a8f5
Jason Swett <jason@benfranklinlabs.com>
2024-11-06 10:22:48 -0500

    Make env var less hard-coded.

--- lib/job_machine_script.rb
+++ lib/job_machine_script.rb
@@ -115,27 +115,30 @@ module JobMachineScript
   end
 
   class TestSuiteCommand
-    def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:)
+    def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:, docker_compose_env_vars:)
       @registry_cache_image_url = registry_cache_image_url
       @test_files_string = test_files_string
       @rspec_seed = rspec_seed
       @test_output_filename = test_output_filename
+      @docker_compose_env_vars = docker_compose_env_vars
     end
 
     def to_s
       "script -f #{@test_output_filename} -c \"sudo #{script_env_vars} #{docker_compose_command.strip}\""
     end
 
+    def docker_compose_command
+      "docker-compose -f .saturnci/docker-compose.yml run -e #{docker_compose_env_vars} saturn_test_app #{rspec_command}"
+    end
+
     private
 
     def script_env_vars
-      {
-        "SATURN_TEST_APP_IMAGE_URL" => @registry_cache_image_url,
-      }.map { |key, value| "#{key}=#{value}" }.join(" ")
+      "SATURN_TEST_APP_IMAGE_URL=#{@registry_cache_image_url}"
     end
 
-    def docker_compose_command
-      "docker-compose -f .saturnci/docker-compose.yml run -e ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY=hJWGiKGSmeeZeUSxkB6uJOeE9mi5MRmD saturn_test_app #{rspec_command}"
+    def docker_compose_env_vars
+      @docker_compose_env_vars.map { |key, value| "#{key}=#{value}" }.join(" ")
     end
 
     def rspec_command
@@ -263,7 +266,10 @@ if ENV["JOB_ID"]
     registry_cache_image_url: registry_cache_image_url,
     test_files_string: test_files_string,
     rspec_seed: ENV["RSPEC_SEED"],
-    test_output_filename: TEST_OUTPUT_FILENAME
+    test_output_filename: TEST_OUTPUT_FILENAME,
+    docker_compose_env_vars: {
+      "ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY" => "hJWGiKGSmeeZeUSxkB6uJOeE9mi5MRmD"
+    }
   ).to_s
   puts command
 
--- spec/lib/job_machine_script/job_machine_script_spec.rb
+++ spec/lib/job_machine_script/job_machine_script_spec.rb
@@ -2,19 +2,26 @@ require "rails_helper"
 
 RSpec.describe "job machine script" do
   describe "TestSuiteCommand" do
-    let!(:rspec_command) do
+    let!(:command) do
       JobMachineScript::TestSuiteCommand.new(
         registry_cache_image_url: "registrycache.saturnci.com:5000/saturn_test_app:123456",
         test_files_string: "spec/models/github_token_spec.rb spec/rebuilds_spec.rb spec/sign_up_spec.rb spec/test_spec.rb",
         rspec_seed: "999",
-        test_output_filename: "tmp/test_output.txt"
+        test_output_filename: "tmp/test_output.txt",
+        docker_compose_env_vars: { "FOO" => "bar" }
       )
     end
 
+    describe "docker_compose_command" do
+      it "returns a command" do
+        expect(command.docker_compose_command).to eq("docker-compose -f .saturnci/docker-compose.yml run -e FOO=bar saturn_test_app bundle exec rspec --require ./example_status_persistence.rb --format=documentation --order rand:999 spec/models/github_token_spec.rb spec/rebuilds_spec.rb spec/sign_up_spec.rb spec/test_spec.rb")
+      end
+    end
+
     it "returns a command" do
-      docker_compose_command = "docker-compose -f .saturnci/docker-compose.yml run saturn_test_app bundle exec rspec --require ./example_status_persistence.rb --format=documentation --order rand:999 spec/models/github_token_spec.rb spec/rebuilds_spec.rb spec/sign_up_spec.rb spec/test_spec.rb"
-      script_env_vars = "SATURN_TEST_APP_IMAGE_URL=registrycache.saturnci.com:5000/saturn_test_app:123456 ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY=hJWGiKGSmeeZeUSxkB6uJOeE9mi5MRmD"
-      expect(rspec_command.to_s).to eq("script -f tmp/test_output.txt -c \"sudo #{script_env_vars} #{docker_compose_command}\"")
+      docker_compose_command = "docker-compose -f .saturnci/docker-compose.yml run -e FOO=bar saturn_test_app bundle exec rspec --require ./example_status_persistence.rb --format=documentation --order rand:999 spec/models/github_token_spec.rb spec/rebuilds_spec.rb spec/sign_up_spec.rb spec/test_spec.rb"
+      script_env_vars = "SATURN_TEST_APP_IMAGE_URL=registrycache.saturnci.com:5000/saturn_test_app:123456"
+      expect(command.to_s).to eq("script -f tmp/test_output.txt -c \"sudo #{script_env_vars} #{docker_compose_command}\"")
     end
   end
 end
