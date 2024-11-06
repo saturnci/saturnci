609974962df76cc1397d6519f35408a8469c58d0
Jason Swett <jason@benfranklinlabs.com>
2024-11-06 10:53:50 -0500

    Squashed commit of the following:
    commit a6d79b3f3f0198faed7a7c565db6db58d9f8dff1
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:53:34 2024 -0500
    
        Remove env var debug output.
    
    commit 00d5ec98c4c04d8b79b79756c97aedd97561a27d
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:53:09 2024 -0500
    
        Remove hard-coded env vars.
    
    commit 0a07d42f55cf154ea75ca25e5af96f16370871b8
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:43:18 2024 -0500
    
        Move hard-coded values out of job script.
    
    commit 0ed05e282d8b74d2dc579b422f1c6754de3c5490
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:36:06 2024 -0500
    
        Fix bug.
    
    commit b209d1877995047923c6492400b5c10756a2d1e7
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:23:36 2024 -0500
    
        Move remaining env vars.
    
    commit 1494c669a264c8a8dcc013e3aca0d17727ffb5a2
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:22:48 2024 -0500
    
        Make env var less hard-coded.
    
    commit cf0fd8ae15fcb7cf0ba2898df05bf683351963b5
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 10:11:30 2024 -0500
    
        Get env var passing working.
    
    commit 2664f8b9b0f61e6be75ef2e370e4706e3233837c
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 09:56:59 2024 -0500
    
        Add env var output.
    
    commit 0cfd3876566a049d3fc24c521667913ab5cc2f25
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 09:50:51 2024 -0500
    
        Revert "Remove env var as a test."
    
        This reverts commit def9f5b1409d8812278c9b79f3639c4a95786198.
    
    commit def9f5b1409d8812278c9b79f3639c4a95786198
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Nov 6 09:49:54 2024 -0500
    
        Remove env var as a test.

--- lib/job_machine_script.rb
+++ lib/job_machine_script.rb
@@ -115,27 +115,30 @@ module JobMachineScript
   end
 
   class TestSuiteCommand
-    def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:)
+    def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:, docker_compose_env_vars:)
       @registry_cache_image_url = registry_cache_image_url
       @test_files_string = test_files_string
       @rspec_seed = rspec_seed
       @test_output_filename = test_output_filename
+      @docker_compose_env_vars = docker_compose_env_vars
     end
 
     def to_s
       "script -f #{@test_output_filename} -c \"sudo #{script_env_vars} #{docker_compose_command.strip}\""
     end
 
+    def docker_compose_command
+      "docker-compose -f .saturnci/docker-compose.yml run #{docker_compose_env_vars} saturn_test_app #{rspec_command}"
+    end
+
     private
 
     def script_env_vars
-      {
-        "SATURN_TEST_APP_IMAGE_URL" => @registry_cache_image_url
-      }.map { |key, value| "#{key}=#{value}" }.join(" ")
+      "SATURN_TEST_APP_IMAGE_URL=#{@registry_cache_image_url}"
     end
 
-    def docker_compose_command
-      "docker-compose -f .saturnci/docker-compose.yml run saturn_test_app #{rspec_command}"
+    def docker_compose_env_vars
+      @docker_compose_env_vars.map { |key, value| "-e #{key}=#{value}" }.join(" ")
     end
 
     def rspec_command
@@ -263,7 +266,8 @@ if ENV["JOB_ID"]
     registry_cache_image_url: registry_cache_image_url,
     test_files_string: test_files_string,
     rspec_seed: ENV["RSPEC_SEED"],
-    test_output_filename: TEST_OUTPUT_FILENAME
+    test_output_filename: TEST_OUTPUT_FILENAME,
+    docker_compose_env_vars: ENV["USER_ENV_VAR_KEYS"].split(",").map { |key| [key, ENV[key]] }.to_h
   ).to_s
   puts command
 
