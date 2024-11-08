0d150e6588c0e8ad0a26488f14886b1b996385c9
Jason Swett <jason@benfranklinlabs.com>
2024-11-08 08:31:52 -0500

    Move SaturnCIWorkerAPI code into separate file.

--- lib/job_machine_script.rb
+++ lib/job_machine_script.rb
@@ -1,154 +1,3 @@
-require "net/http"
-require "uri"
-require "json"
-require "digest"
-require "fileutils"
-require "base64"
-require "open3"
-
-PROJECT_DIR = "/home/ubuntu/project"
-TEST_OUTPUT_FILENAME = "tmp/test_output.txt"
-TEST_RESULTS_FILENAME = "tmp/test_results.txt"
-
-module JobMachineScript
-  class Request
-    def initialize(host, method, endpoint, payload = nil)
-      @host = host
-      @method = method
-      @endpoint = endpoint
-      @payload = payload
-    end
-
-    def execute
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true if url.scheme == "https"
-      http.request(request)
-    end
-
-    def request
-      case @method
-      when :post
-        r = Net::HTTP::Post.new(url)
-      when :delete
-        r = Net::HTTP::Delete.new(url)
-      end
-
-      r.basic_auth(ENV["SATURNCI_API_USERNAME"], ENV["SATURNCI_API_PASSWORD"])
-      r["Content-Type"] = "application/json"
-      r.body = @payload.to_json if @payload
-      r
-    end
-
-    private
-
-    def url
-      URI("#{@host}/api/v1/#{@endpoint}")
-    end
-  end
-
-  class ContentRequest
-    def initialize(host:, api_path:, content_type:, content:)
-      @host = host
-      @api_path = api_path
-      @content_type = content_type
-      @content = content
-    end
-
-    def execute
-      command = <<~COMMAND
-        curl -s -f -u #{ENV["SATURNCI_API_USERNAME"]}:#{ENV["SATURNCI_API_PASSWORD"]} \
-            -X POST \
-            -H "Content-Type: #{@content_type}" \
-            -d "#{@content}" #{url}
-      COMMAND
-
-      system(command)
-    end
-
-    private
-
-    def url
-      "#{@host}/api/v1/#{@api_path}"
-    end
-  end
-
-  class FileContentRequest
-    def initialize(host:, api_path:, content_type:, file_path:)
-      @host = host
-      @api_path = api_path
-      @content_type = content_type
-      @file_path = file_path
-    end
-
-    def execute
-      command = <<~COMMAND
-        curl -s -f -u #{ENV["SATURNCI_API_USERNAME"]}:#{ENV["SATURNCI_API_PASSWORD"]} \
-            -X POST \
-            -H "Content-Type: #{@content_type}" \
-            --data-binary "@#{@file_path}" #{url}
-      COMMAND
-
-      system(command)
-    end
-
-    private
-
-    def url
-      "#{@host}/api/v1/#{@api_path}"
-    end
-  end
-
-  class Client
-    def initialize(host)
-      @host = host
-    end
-
-    def post(endpoint, payload = nil)
-      Request.new(@host, :post, endpoint, payload).execute
-    end
-
-    def delete(endpoint)
-      Request.new(@host, :delete, endpoint).execute
-    end
-
-    def debug(message)
-      post("debug_messages", message)
-    end
-  end
-
-  class TestSuiteCommand
-    def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:, docker_compose_env_vars:)
-      @registry_cache_image_url = registry_cache_image_url
-      @test_files_string = test_files_string
-      @rspec_seed = rspec_seed
-      @test_output_filename = test_output_filename
-      @docker_compose_env_vars = docker_compose_env_vars
-    end
-
-    def to_s
-      "script -f #{@test_output_filename} -c \"sudo #{script_env_vars} #{docker_compose_command.strip}\""
-    end
-
-    def docker_compose_command
-      "docker-compose -f .saturnci/docker-compose.yml run #{docker_compose_env_vars} saturn_test_app #{rspec_command}"
-    end
-
-    private
-
-    def script_env_vars
-      "SATURN_TEST_APP_IMAGE_URL=#{@registry_cache_image_url}"
-    end
-
-    def docker_compose_env_vars
-      @docker_compose_env_vars.map { |key, value| "-e #{key}=#{value}" }.join(" ")
-    end
-
-    def rspec_command
-      "bundle exec rspec --require ./example_status_persistence.rb --format=documentation --order rand:#{@rspec_seed} #{@test_files_string}"
-    end
-  end
-end
-
 def stream(log_file_path, api_path, client)
   Thread.new do
     most_recent_total_line_count = 0
@@ -157,7 +6,7 @@ def stream(log_file_path, api_path, client)
       all_lines = File.readlines(log_file_path)
       newest_content = all_lines[most_recent_total_line_count..-1].join("\n")
 
-      JobMachineScript::ContentRequest.new(
+      SaturnCIWorkerAPI::ContentRequest.new(
         host: ENV["HOST"],
         api_path: api_path,
         content_type: "text/plain",
@@ -172,7 +21,7 @@ def stream(log_file_path, api_path, client)
 end
 
 if ENV["JOB_ID"]
-  client = JobMachineScript::Client.new(ENV["HOST"])
+  client = SaturnCIWorkerAPI::Client.new(ENV["HOST"])
 
   puts "Starting to stream system logs"
   stream("/var/log/syslog", "jobs/#{ENV["JOB_ID"]}/system_logs", client)
@@ -230,7 +79,7 @@ if ENV["JOB_ID"]
   selected_tests = chunks[ENV['JOB_ORDER_INDEX'].to_i - 1]
   test_files_string = selected_tests.join(' ')
 
-  command = JobMachineScript::TestSuiteCommand.new(
+  command = SaturnCIWorkerAPI::TestSuiteCommand.new(
     registry_cache_image_url: registry_cache_image_url,
     test_files_string: test_files_string,
     rspec_seed: ENV["RSPEC_SEED"],
@@ -247,7 +96,7 @@ if ENV["JOB_ID"]
   client.post("jobs/#{ENV["JOB_ID"]}/job_finished_events")
 
   puts "Sending report"
-  test_reports_request = JobMachineScript::FileContentRequest.new(
+  test_reports_request = SaturnCIWorkerAPI::FileContentRequest.new(
     host: ENV["HOST"],
     api_path: "jobs/#{ENV["JOB_ID"]}/test_reports",
     content_type: "text/plain",
