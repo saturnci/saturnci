e42d5dceb44ad7d878d79edc4ea791d9c6b7bf87
Jason Swett <jason@benfranklinlabs.com>
2024-11-09 12:56:49 -0500

    Move ContentRequest.

---
+++ lib/saturnci_job_api/content_request.rb
@@ -0,0 +1,27 @@
+module SaturnCIJobAPI
+  class ContentRequest
+    def initialize(host:, api_path:, content_type:, content:)
+      @host = host
+      @api_path = api_path
+      @content_type = content_type
+      @content = content
+    end
+
+    def execute
+      command = <<~COMMAND
+        curl -s -f -u #{ENV["SATURNCI_API_USERNAME"]}:#{ENV["SATURNCI_API_PASSWORD"]} \
+            -X POST \
+            -H "Content-Type: #{@content_type}" \
+            -d "#{@content}" #{url}
+      COMMAND
+
+      system(command)
+    end
+
+    private
+
+    def url
+      "#{@host}/api/v1/#{@api_path}"
+    end
+  end
+end
