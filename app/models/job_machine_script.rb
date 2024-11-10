9cad70c2082f7eff1b8c93cb65e5fd68b0907d40
Jason Swett <jason@benfranklinlabs.com>
2024-11-09 21:21:25 -0500

    Separate more files.

--- app/models/job_machine_script.rb
+++ app/models/job_machine_script.rb
@@ -5,11 +5,15 @@ class JobMachineScript
   end
 
   def library_content
+    client_filename = File.join(Rails.root, "lib", "saturnci_job_api", "client.rb")
+    file_content_request_filename = File.join(Rails.root, "lib", "saturnci_job_api", "file_content_request.rb")
     content_request_filename = File.join(Rails.root, "lib", "saturnci_job_api", "content_request.rb")
     request_filename = File.join(Rails.root, "lib", "saturnci_job_api", "request.rb")
     script_filename = File.join(Rails.root, "lib", "saturnci_job_api.rb")
 
-    File.read(content_request_filename) +
+    File.read(client_filename) +
+      File.read(file_content_request_filename) +
+      File.read(content_request_filename) +
       File.read(request_filename) +
       File.read(script_filename)
   end
--- lib/saturnci_job_api.rb
+++ lib/saturnci_job_api.rb
@@ -11,50 +11,6 @@ TEST_OUTPUT_FILENAME = "tmp/test_output.txt"
 TEST_RESULTS_FILENAME = "tmp/test_results.txt"
 
 module SaturnCIJobAPI
-  class FileContentRequest
-    def initialize(host:, api_path:, content_type:, file_path:)
-      @host = host
-      @api_path = api_path
-      @content_type = content_type
-      @file_path = file_path
-    end
-
-    def execute
-      command = <<~COMMAND
-        curl -s -f -u #{ENV["SATURNCI_API_USERNAME"]}:#{ENV["SATURNCI_API_PASSWORD"]} \
-            -X POST \
-            -H "Content-Type: #{@content_type}" \
-            --data-binary "@#{@file_path}" #{url}
-      COMMAND
-
-      system(command)
-    end
-
-    private
-
-    def url
-      "#{@host}/api/v1/#{@api_path}"
-    end
-  end
-
-  class Client
-    def initialize(host)
-      @host = host
-    end
-
-    def post(endpoint, payload = nil)
-      Request.new(@host, :post, endpoint, payload).execute
-    end
-
-    def delete(endpoint)
-      Request.new(@host, :delete, endpoint).execute
-    end
-
-    def debug(message)
-      post("debug_messages", message)
-    end
-  end
-
   class TestSuiteCommand
     def initialize(registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:, docker_compose_env_vars:)
       @registry_cache_image_url = registry_cache_image_url
