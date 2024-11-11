218d0419c17683c9bdb9629d8a30c0cfc0466852
Jason Swett <jason@benfranklinlabs.com>
2024-11-10 20:32:23 -0500

    Add stuff to DockerComposeConfiguration.

--- lib/saturnci_job_api/docker_compose_configuration.rb
+++ lib/saturnci_job_api/docker_compose_configuration.rb
@@ -1,4 +1,16 @@
 module SaturnCIJobAPI
   class DockerComposeConfiguration
+    def initialize(registry_cache_image_url:, env_vars:)
+      @registry_cache_image_url = registry_cache_image_url
+      @env_vars = env_vars
+    end
+
+    def env_vars
+      @env_vars.map { |key, value| "-e #{key}=#{value}" }.join(" ")
+    end
+
+    def script_env_vars
+      "SATURN_TEST_APP_IMAGE_URL=#{@registry_cache_image_url}"
+    end
   end
 end
--- lib/saturnci_job_api/test_suite_command.rb
+++ lib/saturnci_job_api/test_suite_command.rb
@@ -1,31 +1,21 @@
 module SaturnCIJobAPI
   class TestSuiteCommand
-    def initialize(docker_compose_configuration:, registry_cache_image_url:, test_files_string:, rspec_seed:, test_output_filename:, docker_compose_env_vars:)
+    def initialize(docker_compose_configuration:, test_files_string:, rspec_seed:, test_output_filename:)
       @docker_compose_configuration = docker_compose_configuration
-      @registry_cache_image_url = registry_cache_image_url
       @test_files_string = test_files_string
       @rspec_seed = rspec_seed
       @test_output_filename = test_output_filename
-      @docker_compose_env_vars = docker_compose_env_vars
     end
 
     def to_s
-      "script -f #{@test_output_filename} -c \"sudo #{script_env_vars} #{docker_compose_command.strip}\""
+      "script -f #{@test_output_filename} -c \"sudo #{@docker_compose_configuration.script_env_vars} #{docker_compose_command.strip}\""
     end
 
     def docker_compose_command
-      "docker-compose -f .saturnci/docker-compose.yml run #{docker_compose_env_vars} saturn_test_app #{rspec_command}"
+      "docker-compose -f .saturnci/docker-compose.yml run #{@docker_compose_configuration.env_vars} saturn_test_app #{rspec_command}"
     end
 
     private
-
-    def script_env_vars
-      "SATURN_TEST_APP_IMAGE_URL=#{@registry_cache_image_url}"
-    end
-
-    def docker_compose_env_vars
-      @docker_compose_env_vars.map { |key, value| "-e #{key}=#{value}" }.join(" ")
-    end
 
     def rspec_command
       "bundle exec rspec --require ./example_status_persistence.rb --format=documentation --order rand:#{@rspec_seed} #{@test_files_string}"
