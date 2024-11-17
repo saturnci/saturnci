2bd57061bfde89a2d85fba49b00183185c7a5db7
Jason Swett <jason@benfranklinlabs.com>
2024-11-16 21:52:16 -0500

    Fix project secret bugs.

--- app/models/project_secret_collection.rb
+++ app/models/project_secret_collection.rb
@@ -4,21 +4,15 @@ class ProjectSecretCollection
   attr_accessor :project_secrets
 
   def project_secrets_attributes=(attributes)
-    @project_secrets = []
-
-    attributes.each do |_index, project_secret_attributes|
-      @project_secrets << ProjectSecret.new(project_secret_attributes)
-    end
+    @payload = ProjectSecretCollectionPayload.new(
+      attributes,
+      persisted_project_secrets: project.project_secrets
+    )
   end
 
   def save!
-    project_secrets = @project_secrets.reject do |project_secret|
-      (project_secret.key.blank? || project_secret.value.blank?) ||
-        project_secret.value == ProjectSecret::MASK_VALUE
-    end
-
-    ProjectSecret.where(key: project_secrets.map(&:key)).destroy_all
-
-    project.project_secrets << project_secrets
+    project.project_secrets.where(key: @payload.blank_keys).destroy_all
+    project.project_secrets.where(key: @payload.touched_keys).destroy_all
+    project.project_secrets << @payload.touched_project_secrets
   end
 end
