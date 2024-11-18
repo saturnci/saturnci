dae268150bc6751b730496dab7670e93879fe5dc
Jason Swett <jason@benfranklinlabs.com>
2024-11-17 21:14:05 -0500

    Rename billing stuff.

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -2,11 +2,11 @@ class BillingController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
 
-    @jobs = BillingReport.new(
+    @runs = BillingReport.new(
       project: @project,
       year: params[:year] || Time.current.year,
       month: params[:month] || Time.current.month
-    ).jobs
+    ).runs
 
     @billing_navigation_component = BillingNavigationComponent.new(project: @project)
   end
