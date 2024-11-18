bfa338b0b691d9f7753fda52af30d7d94dda18c0
Jason Swett <jason@benfranklinlabs.com>
2024-11-17 21:07:41 -0500

    Rename JobDecorator to RunDecorator.

--- spec/models/billing/run_decorator_spec.rb
+++ spec/models/billing/run_decorator_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
-RSpec.describe Billing::JobDecorator do
-  let!(:job) { create(:job) }
+RSpec.describe Billing::RunDecorator do
+  let!(:run) { create(:run) }
 
   describe "#charge" do
     before do
-      allow(job).to receive(:duration).and_return(60)
+      allow(run).to receive(:duration).and_return(60)
     end
 
     it "returns the charge" do
-      decorated_job = Billing::JobDecorator.new(job)
-      expect(decorated_job.charge).to eq(0.06)
+      decorated_run = Billing::RunDecorator.new(run)
+      expect(decorated_run.charge).to eq(0.06)
     end
   end
 end
