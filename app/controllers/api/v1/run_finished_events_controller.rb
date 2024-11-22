3040477a71ebd95c9e5e0ea812acad8a19177331
Jason Swett <jason@benfranklinlabs.com>
2024-11-22 09:16:47 -0500

    Rename JobFinishedEventsController to RunFinishedEventsController.

---
+++ app/controllers/api/v1/run_finished_events_controller.rb
@@ -0,0 +1,24 @@
+module API
+  module V1
+    class RunFinishedEventsController < APIController
+      def create
+        run = Run.find(params[:job_id])
+
+        ActiveRecord::Base.transaction do
+          run.finish!
+
+          if run.build.runs.all?(&:finished?)
+            Turbo::StreamsChannel.broadcast_update_to(
+              "build_status_#{run.build.id}",
+              target: "build_status_#{run.build.id}",
+              partial: "builds/list_item",
+              locals: { build: run.build }
+            )
+          end
+        end
+
+        head :ok
+      end
+    end
+  end
+end
