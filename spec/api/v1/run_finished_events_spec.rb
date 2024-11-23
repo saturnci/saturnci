8d606cfc9cb4dd28687ce4f077268c900babe991
Jason Swett <jason@benfranklinlabs.com>
2024-11-22 20:56:26 -0500

    Fix test.

--- spec/api/v1/run_finished_events_spec.rb
+++ spec/api/v1/run_finished_events_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 include APIAuthenticationHelper
 
 RSpec.describe "run finished events", type: :request do
-  describe "POST /api/v1/jobs/:id/run_finished_events" do
+  describe "POST /api/v1/runs/:id/run_finished_events" do
     let!(:run) { create(:run) }
 
     it "increases the count of run events by 1" do
       expect {
         post(
-          api_v1_job_run_finished_events_path(run),
+          api_v1_run_run_finished_events_path(run),
           headers: api_authorization_headers
         )
       }.to change(RunEvent, :count).by(1)
@@ -16,7 +16,7 @@ RSpec.describe "run finished events", type: :request do
 
     it "returns an empty 200 response" do
       post(
-        api_v1_job_run_finished_events_path(run),
+        api_v1_run_run_finished_events_path(run),
         headers: api_authorization_headers
       )
       expect(response).to have_http_status(200)
@@ -26,7 +26,7 @@ RSpec.describe "run finished events", type: :request do
     it "creates a charge for the run" do
       expect {
         post(
-          api_v1_job_run_finished_events_path(run),
+          api_v1_run_run_finished_events_path(run),
           headers: api_authorization_headers
         )
       }.to change { run.reload.charge.present? }.from(false).to(true)
