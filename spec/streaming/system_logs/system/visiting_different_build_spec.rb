71cdebb720d00306c23d48ec242d4a22cbed6b8e
Jason Swett <jason@benfranklinlabs.com>
2024-12-02 09:54:12 -0500

    Squashed commit of the following:
    commit 4212d3e3bac149acda65768044ab0dfe66224c65
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Dec 2 09:50:22 2024 -0500
    
        Fix more tests.
    
    commit 31399c3a0101479b5e4b137cbdd01aab22ce9d42
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Dec 2 09:46:11 2024 -0500
    
        Fix tests.
    
    commit 430fe6ae5897988ff90489e42e173d7d6a09beed
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Dec 1 21:28:00 2024 -0500
    
        Fix partial.
    
    commit 59004e343492986cc1887f08a42e9f87f4425994
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Dec 1 21:25:13 2024 -0500
    
        Remove job routes.

--- spec/streaming/system_logs/system/visiting_different_build_spec.rb
+++ spec/streaming/system_logs/system/visiting_different_build_spec.rb
@@ -5,20 +5,20 @@ describe "Visiting different build", type: :system do
   include SaturnAPIHelper
   include NavigationHelper
 
-  let!(:original_job) do
-    create(:job, system_logs: "original system log content")
+  let!(:original_run) do
+    create(:run, system_logs: "original system log content")
   end
 
   before do
-    login_as(original_job.build.project.user, scope: :user)
-    visit job_path(original_job, "system_logs")
+    login_as(original_run.build.project.user, scope: :user)
+    visit run_path(original_run, "system_logs")
   end
 
   context "visiting a different build" do
-    let!(:other_job) do
-      create(:job, system_logs: "other job system logs") do |j|
+    let!(:other_run) do
+      create(:run, system_logs: "other run system logs") do |j|
         j.build.update!(
-          project: original_job.build.project,
+          project: original_run.build.project,
           commit_message: "Make other change."
         )
       end
@@ -26,18 +26,18 @@ describe "Visiting different build", type: :system do
 
     context "after log update occurs" do
       before do
-        visit_build_tab("system_logs", job: original_job)
-        navigate_to_build(other_job.build)
-        navigate_to_build_tab("system_logs", job: other_job)
-        system_log_http_request(run: original_job, body: "new system log content")
+        visit_build_tab("system_logs", run: original_run)
+        navigate_to_build(other_run.build)
+        navigate_to_build_tab("system_logs", run: other_run)
+        system_log_http_request(run: original_run, body: "new system log content")
       end
 
-      it "does not show original job's system logs on the other job's system logs tab" do
-        expect(page).not_to have_content(original_job.system_logs)
+      it "does not show original run's system logs on the other run's system logs tab" do
+        expect(page).not_to have_content(original_run.system_logs)
       end
 
-      it "shows the other job's system logs on the other job's system logs tab" do
-        expect(page).to have_content(other_job.system_logs)
+      it "shows the other run's system logs on the other run's system logs tab" do
+        expect(page).to have_content(other_run.system_logs)
       end
     end
   end
