7084d5862daa4288c6e54faa86399f8eb1e032fd
Jason Swett <jason@benfranklinlabs.com>
2025-01-19 09:40:29 -0500

    Squashed commit of the following:
    commit e131fafe105d0a79e1505554615034bd8c6e1bff
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Jan 19 09:39:59 2025 -0500
    
        Fix test.
    
    commit b7a80a43cc2b287a11700f2feab236be3627009a
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Jan 19 09:39:14 2025 -0500
    
        Add comment.
    
    commit 1cf4ab85083dc4c7b97cba5a9d43c5324ccefc9e
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Jan 19 09:30:36 2025 -0500
    
        Rename saturn_installations.name to account_name.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_22_132129) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_19_142839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -101,7 +101,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_132129) do
     t.string "github_installation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "account_name"
     t.index ["user_id", "github_installation_id"], name: "index_saturn_installations_on_user_and_github_id", unique: true
     t.index ["user_id"], name: "index_saturn_installations_on_user_id"
   end
