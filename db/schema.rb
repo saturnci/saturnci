85e02ef6c5d5b6b3a4ee05bc64174422722835ca
Jason Swett <jason@benfranklinlabs.com>
2025-01-22 18:51:28 -0500

    Make saturn_installations.account_name not nullable.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_19_152634) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_22_235048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -103,7 +103,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_19_152634) do
     t.string "github_installation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "account_name"
+    t.string "account_name", null: false
     t.datetime "deleted_at"
     t.index ["user_id", "github_installation_id"], name: "index_saturn_installations_on_user_and_github_id", unique: true
     t.index ["user_id"], name: "index_saturn_installations_on_user_id"
