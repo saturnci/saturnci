10cb593c0307dd08ba1a259cedf76b8912b82315
Jason Swett <jason@benfranklinlabs.com>
2025-01-27 16:50:29 -0500

    Get account deletion working.

--- app/models/github_account.rb
+++ app/models/github_account.rb
@@ -1,3 +1,6 @@
+require 'jwt'
+require 'octokit'
+
 # A GitHubAccount is created via a GitHub "created" webhook
 # event, which is handled in an object called GitHub::Installation.
 class GitHubAccount < ApplicationRecord
@@ -6,7 +9,16 @@ class GitHubAccount < ApplicationRecord
   has_many :projects, dependent: :destroy
 
   def octokit_client
-    bearer_token = GitHubToken.generate(github_installation_id)
-    Octokit::Client.new(bearer_token: bearer_token)
+    private_pem = Rails.configuration.github_private_pem
+    private_key = OpenSSL::PKey::RSA.new(private_pem)
+
+    payload = {
+      iat: Time.now.to_i,  # Issued at time
+      exp: Time.now.to_i + (10 * 60),  # JWT expiration time (10 minutes)
+      iss: ENV['GITHUB_APP_ID']
+    }
+
+    jwt = JWT.encode(payload, private_key, 'RS256')
+    Octokit::Client.new(bearer_token: jwt)
   end
 end
