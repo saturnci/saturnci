4568bd6988a56aaf2505d83dc74469053b538d84
Jason Swett <jason@benfranklinlabs.com>
2025-01-31 18:09:04 -0500

    Add verify_authorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user_or_404!, unless: :devise_controller?
+  after_action :verify_authorized, except: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
