51266a3d2653cf72963950f7ae6dacde5b682372
Jason Swett <jason@benfranklinlabs.com>
2025-01-31 21:53:48 -0500

    Skip verify_authorized for API controllers.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,7 @@ module API
     class APIController < ApplicationController
       skip_before_action :verify_authenticity_token
       skip_before_action :authenticate_user_or_404!
+      skip_after_action :verify_authorized
 
       http_basic_authenticate_with(
         name: ENV["SATURNCI_API_USERNAME"],
