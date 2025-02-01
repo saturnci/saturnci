3a760e5d031df8689f6b97ea1a71f6c4737acc4f
Jason Swett <jason@benfranklinlabs.com>
2025-02-01 12:11:04 -0500

    Make API auth work via user id/token.

---
+++ spec/api/v1/authentication_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe "API::V1::YourEndpoint", type: :request do
+  let!(:user) { create(:user) }
+
+  describe "GET /api/v1/builds" do
+    context "with valid credentials" do
+      it "returns success" do
+        credentials = ActionController::HttpAuthentication::Basic.encode_credentials(user.id.to_s, user.api_token)
+
+        get(
+          api_v1_builds_path,
+          headers: { "Authorization" => credentials }
+        )
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "with invalid credentials" do
+      it "returns unauthorized" do
+        credentials = ActionController::HttpAuthentication::Basic.encode_credentials(user.id.to_s, "wrongtoken")
+
+        get(
+          api_v1_builds_path,
+          headers: { "Authorization" => credentials }
+        )
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "invalid user id" do
+      it "returns unauthorized" do
+        credentials = ActionController::HttpAuthentication::Basic.encode_credentials("wrongid", user.api_token)
+
+        get(
+          api_v1_builds_path,
+          headers: { "Authorization" => credentials }
+        )
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "with missing credentials" do
+      it "returns unauthorized" do
+        get api_v1_builds_path
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+end
