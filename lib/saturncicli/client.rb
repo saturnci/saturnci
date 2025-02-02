adf1f9414597ca78bdb4b4ab3759d6ce69c58711
Jason Swett <jason@benfranklinlabs.com>
2025-02-02 09:33:45 -0500

    Fix CLI by adding SSL config.

--- lib/saturncicli/client.rb
+++ lib/saturncicli/client.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
-require 'json'
-require_relative 'api_request'
-require_relative 'display/table'
-require_relative 'ssh_session'
-require_relative 'connection_details'
+require "json"
+require_relative "api_request"
+require_relative "display/table"
+require_relative "ssh_session"
+require_relative "connection_details"
 
 module SaturnCICLI
   class Client
-    DEFAULT_HOST = 'http://localhost:3000'
+    DEFAULT_HOST = "https://app.saturnci.com"
     attr_reader :host, :username, :password
 
     def initialize(username:, password:, host: DEFAULT_HOST)
       @username = username
       @password = password
       @host = host
-      request('builds')
     end
 
     def execute(argument)
       case argument
       when /--job\s+(\S+)/
-        job_id = argument.split(' ')[1]
+        job_id = argument.split(" ")[1]
         ssh(job_id)
-      when 'jobs'
+      when "jobs"
         jobs
-      when 'builds'
+      when "builds"
         builds
       when nil
         builds
@@ -35,7 +34,7 @@ module SaturnCICLI
     end
 
     def builds(options = {})
-      response = request('builds')
+      response = request("builds")
       builds = JSON.parse(response.body)
 
       puts Display::Table.new(
@@ -46,7 +45,7 @@ module SaturnCICLI
     end
 
     def jobs(options = {})
-      response = request('jobs')
+      response = request("jobs")
       jobs = JSON.parse(response.body)
 
       puts Display::Table.new(
@@ -62,7 +61,7 @@ module SaturnCICLI
       )
 
       until connection_details.refresh.ip_address
-        print '.'
+        print "."
         sleep(ConnectionDetails::WAIT_INTERVAL_IN_SECONDS)
       end
 
