a35d1b1bf8225c9563cab6b531f8ccf887be7a3f
Jason Swett <jason@benfranklinlabs.com>
2025-02-02 13:58:05 -0500

    Add RSA key content to endpoint.

--- app/controllers/api/v1/runs_controller.rb
+++ app/controllers/api/v1/runs_controller.rb
@@ -3,15 +3,23 @@ module API
     class RunsController < APIController
       def index
         @runs = Run.running
-        render 'index', formats: [:json]
+        render "index", formats: [:json]
       end
 
       def show
         run = Run.find_by_abbreviated_hash(params[:id])
 
         render json: run.as_json.merge(
-          ip_address: RunnerNetwork.new(run.runner_id).ip_address
+          ip_address: RunnerNetwork.new(run.runner_id).ip_address,
+          rsa_key: rsa_key(run)
         )
+      end
+
+      private
+
+      def rsa_key(run)
+        return unless File.exist?(run.runner_rsa_key_path.to_s)
+        File.read(run.runner_rsa_key_path)
       end
     end
   end
