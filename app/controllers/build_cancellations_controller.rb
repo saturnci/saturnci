931f66a2d515ae55ca9cf577f96233a8e66e5a2a
Jason Swett <jason@benfranklinlabs.com>
2025-02-05 12:29:48 -0500

    Fix build cancellation.

--- app/controllers/build_cancellations_controller.rb
+++ app/controllers/build_cancellations_controller.rb
@@ -1,6 +1,8 @@
 class BuildCancellationsController < ApplicationController
   def create
     build = Build.find(params[:build_id])
+    authorize build, :destroy?
+
     build.cancel!
 
     redirect_to build.project
