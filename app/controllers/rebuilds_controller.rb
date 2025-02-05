8dcf7604757751e962267e8debae0c71cb40c2b6
Jason Swett <jason@benfranklinlabs.com>
2025-02-05 17:06:03 -0500

    Fix rebuilds.

--- app/controllers/rebuilds_controller.rb
+++ app/controllers/rebuilds_controller.rb
@@ -2,6 +2,8 @@ class RebuildsController < ApplicationController
   def create
     original_build = Build.find(params[:build_id])
     build = Rebuild.create!(original_build)
+    authorize build, :create?
+
     build.start!
 
     redirect_to build.project
--- spec/builds/request/rebuilds_spec.rb
+++ spec/builds/request/rebuilds_spec.rb
@@ -11,17 +11,22 @@ describe "rebuilds", type: :request do
     end
 
     before do
+      login_as(build.project.user, scope: :user)
+
       runner_request_double = instance_double(RunnerRequest)
       allow(RunnerRequest).to receive(:new).and_return(runner_request_double)
       allow(runner_request_double).to receive(:execute!)
     end
 
     it "increases the count of builds by 1" do
-      login_as(build.project.user, scope: :user)
-
       expect {
         post(rebuilds_path(build_id: build.id))
       }.to change(Build, :count).by(1)
+    end
+
+    it "returns 302 status" do
+      post(rebuilds_path(build_id: build.id))
+      expect(response).to have_http_status(302)
     end
   end
 end
