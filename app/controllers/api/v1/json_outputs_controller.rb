2c5211a00759c243d61ce7e147550dc699e61c96
Jason Swett <jason@benfranklinlabs.com>
2025-02-05 17:43:11 -0500

    Remove base64 encoding.

--- app/controllers/api/v1/json_outputs_controller.rb
+++ app/controllers/api/v1/json_outputs_controller.rb
@@ -1,11 +1,9 @@
-require "base64"
-
 module API
   module V1
     class JSONOutputsController < APIController
       def create
         run = Run.find(params[:run_id])
-        run.update!(json_output: Base64.decode64(request.body.read))
+        run.update!(json_output: request.body.read)
 
         head :ok
       end
