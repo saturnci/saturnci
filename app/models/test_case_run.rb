caca69880832828af2beb8735298ab4fe9724c8a
Jason Swett <jason@benfranklinlabs.com>
2025-02-07 16:50:22 -0500

    Add failed_first scope.

--- app/models/test_case_run.rb
+++ app/models/test_case_run.rb
@@ -2,6 +2,8 @@ class TestCaseRun < ApplicationRecord
   belongs_to :run
   enum :status, %i[passed failed]
 
+  scope :failed_first, -> { order("status desc, path") }
+
   def project
     run.build.project
   end
