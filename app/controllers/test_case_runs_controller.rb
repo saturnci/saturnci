64f7ce139e77dab188accbb853f018b85c007eaf
Jason Swett <jason@benfranklinlabs.com>
2025-02-07 16:00:15 -0500

    Refactoring.

--- app/controllers/test_case_runs_controller.rb
+++ app/controllers/test_case_runs_controller.rb
@@ -2,5 +2,13 @@ class TestCaseRunsController < ApplicationController
   def show
     @test_case_run = TestCaseRun.find(params[:id])
     authorize @test_case_run
+
+    @build_component = BuildComponent.new(
+      build: @test_case_run.run.build,
+      current_tab_name: "overview",
+      branch_name: params[:branch_name],
+      statuses: params[:statuses],
+      clear: params[:clear]
+    )
   end
 end
