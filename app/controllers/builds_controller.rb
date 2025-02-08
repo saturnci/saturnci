91bfdb798458d773d6110cf95ff9c9bcc7d0aa7e
Jason Swett <jason@benfranklinlabs.com>
2025-02-07 21:43:10 -0500

    Add Overview redirects.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -13,9 +13,25 @@ class BuildsController < ApplicationController
     @build = Build.find(params[:id])
     authorize @build
 
+    if @build.test_case_runs.any?
+      test_case_run = @build.test_case_runs.failed_first.first
+    end
+
     if turbo_frame_request?
-      render partial: "builds/detail", locals: { build: @build }
+      render(
+        partial: "builds/detail",
+        locals: {
+          build: @build,
+          test_case_run: test_case_run
+        }
+      )
+
       return
+    end
+
+    if @build.test_case_runs.any?
+      test_case_run = @build.test_case_runs.failed_first.first
+      redirect_to project_test_case_run_path(@build.project, test_case_run) and return
     end
 
     @build_component = BuildComponent.new(
--- spec/builds/overview/request/get_test_case_run_spec.rb
+++ spec/builds/overview/request/get_test_case_run_spec.rb
@@ -9,14 +9,28 @@ describe "test case run", type: :request do
     )
   end
 
-  describe "GET project_test_case_run_path" do
-    before do
-      login_as(test_case_run.project.user)
-    end
+  before do
+    login_as(test_case_run.project.user)
+  end
 
-    it "returns a success response" do
+  context "going straight to the test case run page" do
+    it "includes the test case run description" do
       get project_test_case_run_path(test_case_run.project, test_case_run)
       expect(response.body).to include(test_case_run.description)
+    end
+  end
+
+  context "visiting build" do
+    it "redirects to the test case run page" do
+      get project_build_path(test_case_run.project, test_case_run.run.build)
+      expect(response).to redirect_to(project_test_case_run_path(test_case_run.project, test_case_run))
+    end
+
+    context "turbo request" do
+      it "redirects to the test case run page" do
+        get project_build_path(test_case_run.project, test_case_run.run.build), headers: { "Turbo-Frame" => "true" }
+        expect(response.body).to include(test_case_run.description)
+      end
     end
   end
 end
