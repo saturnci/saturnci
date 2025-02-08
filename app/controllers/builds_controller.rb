a771ed433743cc6e87f10a6880945dbd1f8e0cc5
Jason Swett <jason@benfranklinlabs.com>
2025-02-08 16:44:06 -0500

    Fix redirect.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -29,9 +29,13 @@ class BuildsController < ApplicationController
       return
     end
 
-    #if @build.test_case_runs.any?
-      #redirect_to project_test_case_run_path(@build.project, test_case_run) and return
-    #end
+    if @build.test_case_runs.any?
+      redirect_to project_test_case_run_path(
+        @build.project,
+        test_case_run,
+        request.query_parameters
+      ) and return
+    end
 
     @build_component = BuildComponent.new(
       build: @build,
