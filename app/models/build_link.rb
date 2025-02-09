3e3e74afe40008bd4399804d0e99fd1a26438b1a
Jason Swett <jason@benfranklinlabs.com>
2025-02-09 13:58:06 -0500

    Make it so filter gets preserved when clicking on links in build list.

--- app/models/build_link.rb
+++ app/models/build_link.rb
@@ -2,16 +2,21 @@ class BuildLink
   include Rails.application.routes.url_helpers
   DEFAULT_PARTIAL = "test_output"
 
-  def initialize(build)
+  def initialize(build, request_query_parameters = {})
     @build = build
+    @request_query_parameters = request_query_parameters
   end
 
   def path
     Rails.cache.fetch(cache_key) do
       if @build.finished? || @build.runs.empty?
-        project_build_path(@build.project, @build)
+        project_build_path(@build.project, @build, @request_query_parameters)
       else
-        run_path(first_failed_run || @build.runs.sorted.first, DEFAULT_PARTIAL)
+        run_path(
+          first_failed_run || @build.runs.sorted.first,
+          DEFAULT_PARTIAL,
+          @request_query_parameters
+        )
       end
     end
   end
@@ -26,7 +31,8 @@ class BuildLink
     [
       "build_link_path",
       @build.id,
-      @build.updated_at.to_i
+      @build.updated_at.to_i,
+      @request_query_parameters.to_s
     ].join("/")
   end
 end
