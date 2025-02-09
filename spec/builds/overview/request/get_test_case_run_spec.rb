1ba33a19079a122b563df76f3fba9bece4fd7827
Jason Swett <jason@benfranklinlabs.com>
2025-02-09 13:21:28 -0500

    Improve test.

--- spec/builds/overview/request/get_test_case_run_spec.rb
+++ spec/builds/overview/request/get_test_case_run_spec.rb
@@ -1,35 +1,50 @@
 require "rails_helper"
 
 describe "test case run", type: :request do
-  let!(:test_case_run) do
+  let!(:run) { create(:run, :failed) }
+
+  let!(:passed_test_case_run) do
     create(
       :test_case_run,
-      description: "My test",
-      run: create(:run, :passed)
+      run:,
+      status: "passed",
+      description: "My passed test",
+    )
+  end
+
+  let!(:failed_test_case_run) do
+    create(
+      :test_case_run,
+      run:,
+      status: "failed",
+      description: "My failed test",
     )
   end
 
   before do
-    login_as(test_case_run.project.user)
+    login_as(passed_test_case_run.project.user)
   end
 
   context "going straight to the test case run page" do
     it "includes the test case run description" do
-      get project_test_case_run_path(test_case_run.project, test_case_run)
-      expect(response.body).to include(test_case_run.description)
+      get project_test_case_run_path(run.build.project, passed_test_case_run)
+      expect(response.body).to include("My passed test")
     end
   end
 
   context "visiting build" do
-    it "redirects to the test case run page" do
-      get project_build_path(test_case_run.project, test_case_run.run.build)
-      expect(response).to redirect_to(project_test_case_run_path(test_case_run.project, test_case_run))
+    it "redirects to the test case run page for the first failed test case" do
+      get project_build_path(run.build.project, run.build)
+
+      expect(response).to redirect_to(
+        project_test_case_run_path(run.build.project, failed_test_case_run)
+      )
     end
 
     context "turbo request" do
       it "redirects to the test case run page" do
-        get project_build_path(test_case_run.project, test_case_run.run.build), headers: { "Turbo-Frame" => "true" }
-        expect(response.body).to include(test_case_run.description)
+        get project_build_path(run.build.project, run.build), headers: { "Turbo-Frame" => "true" }
+        expect(response.body).to include("My failed test")
       end
     end
   end
