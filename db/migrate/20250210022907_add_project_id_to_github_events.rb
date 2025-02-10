c585214d6bfd7c52778cca546fb94ff0fc016560
Jason Swett <jason@benfranklinlabs.com>
2025-02-10 09:06:58 -0500

    Squashed commit of the following:
    commit fffb710c2b8dec03708d97682b921be697c4e8cf
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Feb 10 09:00:16 2025 -0500
    
        Fix test.
    
    commit 8063bda04907b60aab220d723735695ee2fb1f85
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Feb 10 08:54:11 2025 -0500
    
        Make project_id nullable.
    
    commit 2702fd735960b609a34bd177d60d78a70d0cabd7
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Feb 10 08:53:41 2025 -0500
    
        Fix bug.
    
    commit 5ffa9e97d6685376d6b4f34f68ac1c9950de78b6
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Mon Feb 10 08:42:42 2025 -0500
    
        Fix bug.
    
    commit 94987244c623adb6cdf78c25586d93a77c5883a4
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Feb 9 21:32:58 2025 -0500
    
        Add github_events.project_id.

---
+++ db/migrate/20250210022907_add_project_id_to_github_events.rb
@@ -0,0 +1,6 @@
+class AddProjectIdToGitHubEvents < ActiveRecord::Migration[8.0]
+  def change
+    GitHubEvent.destroy_all
+    add_reference :github_events, :project, type: :uuid, foreign_key: true
+  end
+end
