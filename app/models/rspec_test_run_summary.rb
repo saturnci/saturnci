4309c8354e37fa10077aba5134584631715e7a57
Jason Swett <jason@benfranklinlabs.com>
2025-02-11 08:26:39 -0500

    Add transaction.

--- app/models/rspec_test_run_summary.rb
+++ app/models/rspec_test_run_summary.rb
@@ -5,18 +5,20 @@ class RSpecTestRunSummary
   end
 
   def generate_test_case_runs!
-    examples.each do |example|
-      @run.test_case_runs.create!(
-        identifier: example[:id],
-        path: example[:file_path],
-        status: example[:status].to_s,
-        description: example[:full_description],
-        line_number: example[:line_number],
-        exception: example[:exception],
-        exception_message: example[:exception] && example[:exception][:message].strip,
-        exception_backtrace: example[:exception] && example[:exception][:backtrace].join("\n"),
-        duration: example[:run_time]
-      )
+    ActiveRecord::Base.transaction do
+      examples.each do |example|
+        @run.test_case_runs.create!(
+          identifier: example[:id],
+          path: example[:file_path],
+          status: example[:status].to_s,
+          description: example[:full_description],
+          line_number: example[:line_number],
+          exception: example[:exception],
+          exception_message: example[:exception] && example[:exception][:message].strip,
+          exception_backtrace: example[:exception] && example[:exception][:backtrace].join("\n"),
+          duration: example[:run_time]
+        )
+      end
     end
   end
 
