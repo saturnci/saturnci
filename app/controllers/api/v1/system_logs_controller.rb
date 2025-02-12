3fbc3b741bcd0d22cf4a9f835c70c28ca68fd281
Jason Swett <jason@benfranklinlabs.com>
2025-02-12 15:47:30 -0500

    Revert "Add graceful error handling to SystemLogsController."
    This reverts commit ed7d16651af3d79271b232765e0c8237241fe90b.

--- app/controllers/api/v1/system_logs_controller.rb
+++ app/controllers/api/v1/system_logs_controller.rb
@@ -6,19 +6,12 @@ module API
       TAB_NAME = "system_logs"
 
       def create
-        begin
-          run = Run.find!(params[:run_id])
-          new_content = Base64.decode64(request.body.read)
-          existing_content = run.attributes[TAB_NAME].to_s
-          run.update!(TAB_NAME => existing_content + new_content)
+        run = Run.find(params[:run_id])
+        new_content = Base64.decode64(request.body.read)
+        existing_content = run.attributes[TAB_NAME].to_s
+        run.update!(TAB_NAME => existing_content + new_content)
 
-          Streaming::RunOutputStream.new(run: run, tab_name: TAB_NAME).broadcast
-
-        rescue StandardError => e
-          render(json: { error: e.message }, status: :bad_request)
-          return
-        end
-
+        Streaming::RunOutputStream.new(run: run, tab_name: TAB_NAME).broadcast
         head :ok
       end
     end
