7c0cad9cc2c294340bf7c4bd484ec84910f6de00
Jason Swett <jason@benfranklinlabs.com>
2025-02-12 15:47:37 -0500

    Revert "Convert ContentRequest to use Ruby."
    This reverts commit 09e7092de28f2b4dabf7e648f336a5fe62d8b62a.

--- lib/saturnci_runner_api/content_request.rb
+++ lib/saturnci_runner_api/content_request.rb
@@ -1,6 +1,3 @@
-require "net/http"
-require "uri"
-
 module SaturnCIRunnerAPI
   class ContentRequest
     def initialize(host:, api_path:, content_type:, content:)
@@ -11,15 +8,14 @@ module SaturnCIRunnerAPI
     end
 
     def execute
-      uri = URI(url)
-      request = Net::HTTP::Post.new(uri)
-      request.basic_auth(ENV["USER_ID"], ENV["USER_API_TOKEN"])
-      request["Content-Type"] = @content_type
-      request.body = @content
+      command = <<~COMMAND
+        curl -s -f -u #{ENV["USER_ID"]}:#{ENV["USER_API_TOKEN"]} \
+            -X POST \
+            -H "Content-Type: #{@content_type}" \
+            -d "#{@content}" #{url}
+      COMMAND
 
-      Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-        http.request(request)
-      end
+      system(command)
     end
 
     private
