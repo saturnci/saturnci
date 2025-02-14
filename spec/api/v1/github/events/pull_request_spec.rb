f00d2fcde10c2579937c9680f1e765a5c2f97fb7
Jason Swett <jason@benfranklinlabs.com>
2025-02-14 16:37:47 -0500

    Squashed commit of the following:
    commit 4d8dbfff24bd0fa38fc356ce28f9645485994c79
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Fri Feb 14 16:31:50 2025 -0500
    
        Change check suite to pull request.
    
    commit f95326c16bc33a1df00b663e6f1df1009114c8b8
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Fri Feb 14 16:26:32 2025 -0500
    
        Make it so pull request creates build.

---
+++ spec/api/v1/github/events/pull_request_spec.rb
@@ -0,0 +1,77 @@
+require "rails_helper"
+include APIAuthenticationHelper
+
+describe "Pull Request", type: :request do
+  let!(:project) do
+    create(:project, github_repo_full_name: "user/test") do |project|
+      project.user.github_accounts.create!(
+        github_installation_id: "1111111"
+      )
+    end
+  end
+
+  let!(:payload) do
+    {
+      "action": "opened",
+      "repository": {
+        "id": 123,
+        "name": "test",
+        "full_name": "user/test"
+      },
+      "pull_request": {
+        "id": 456,
+        "title": "Add new feature",
+        "head": {
+          "ref": "feature-branch",
+          "sha": "abc123"
+        },
+        "user": {
+          "login": "contributor"
+        }
+      }
+    }.to_json
+  end
+
+  let!(:headers) do
+    api_authorization_headers(project.user).merge(
+      "CONTENT_TYPE" => "application/json",
+      "X-GitHub-Event" => "pull_request"
+    )
+  end
+
+  describe "pull request opened event" do
+    it "returns 200" do
+      post(
+        "/api/v1/github_events",
+        params: payload,
+        headers: headers
+      )
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "creates a new build for the project" do
+      expect {
+        post(
+          "/api/v1/github_events",
+          params: payload,
+          headers: headers
+        )
+      }.to change { project.builds.count }.by(1)
+    end
+
+    it "sets the correct branch name, commit hash, commit message, and author name for the build" do
+      post(
+        "/api/v1/github_events",
+        params: payload,
+        headers: headers
+      )
+
+      build = Build.last
+      expect(build.branch_name).to eq("feature-branch")
+      expect(build.commit_hash).to eq("abc123")
+      expect(build.commit_message).to eq("Add new feature")
+      expect(build.author_name).to eq("contributor")
+    end
+  end
+end
