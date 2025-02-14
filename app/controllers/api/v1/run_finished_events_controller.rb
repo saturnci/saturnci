53f8ca93ff20a2e1ff178cc164f71eef0e045c8e
Jason Swett <jason@benfranklinlabs.com>
2025-02-14 17:51:55 -0500

    Add rescue.

--- app/controllers/api/v1/run_finished_events_controller.rb
+++ app/controllers/api/v1/run_finished_events_controller.rb
@@ -2,21 +2,26 @@ module API
   module V1
     class RunFinishedEventsController < APIController
       def create
-        run = Run.find(params[:run_id])
+        begin
+          run = Run.find(params[:run_id])
 
-        ActiveRecord::Base.transaction do
-          run.finish!
+          ActiveRecord::Base.transaction do
+            run.finish!
 
-          if run.build.runs.all?(&:finished?)
-            Turbo::StreamsChannel.broadcast_update_to(
-              "build_status_#{run.build.id}",
-              target: "build_status_#{run.build.id}",
-              partial: "builds/build_link_content",
-              locals: { build: run.build }
-            )
+            if run.build.runs.all?(&:finished?)
+              Turbo::StreamsChannel.broadcast_update_to(
+                "build_status_#{run.build.id}",
+                target: "build_status_#{run.build.id}",
+                partial: "builds/build_link_content",
+                locals: { build: run.build }
+              )
 
-            GitHubCheckRun.new(build: run.build).finish!
+              GitHubCheckRun.new(build: run.build).finish!
+            end
           end
+        rescue StandardError => e
+          render(json: { error: e.message }, status: :bad_request)
+          return
         end
 
         head :ok
