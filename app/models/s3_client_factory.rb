54514b4b1f242da4678d1f8f1f2b672a419102a3
Jason Swett <jason@benfranklinlabs.com>
2025-02-13 21:15:28 -0500

    Add S3ClientFactory.

---
+++ app/models/s3_client_factory.rb
@@ -0,0 +1,12 @@
+require "aws-sdk-s3"
+
+class S3ClientFactory
+  def self.client
+    Aws::S3::Client.new(
+      endpoint: "https://nyc3.digitaloceanspaces.com",
+      access_key_id: ENV["DIGITALOCEAN_SPACES_ACCESS_KEY_ID"],
+      secret_access_key: ENV["DIGITALOCEAN_SPACES_SECRET_ACCESS_KEY"],
+      region: "us-east-1"
+    )
+  end
+end
