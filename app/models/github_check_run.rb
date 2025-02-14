c9c49401294f445dd6f9b44141fa3b2231dc8e21
Jason Swett <jason@benfranklinlabs.com>
2025-02-14 17:15:02 -0500

    Make it so GitHub check run id gets persisted.

--- app/models/github_check_run.rb
+++ app/models/github_check_run.rb
@@ -1,16 +1,17 @@
-class GitHubCheckRun
-  def initialize(build:)
-    @build = build
-    @github_client = build.project.github_account.installation_access_octokit_client
-  end
+class GitHubCheckRun < ApplicationRecord
+  belongs_to :build
 
-  def start!
-    response = @github_client.create_check_run(
-      @build.project.github_repo_full_name,
+  def start!(github_client: nil)
+    github_client ||= build.project.github_account.installation_access_octokit_client
+
+    response = github_client.create_check_run(
+      build.project.github_repo_full_name,
       "SaturnCI",
-      @build.commit_hash,
+      build.commit_hash,
       status: "in_progress",
       started_at: Time.now.iso8601
     )
+
+    update!(github_check_run_id: response.id)
   end
 end
