5a1ab3794bc8c231a62933603284bb26782b52ca
Jason Swett <jason@benfranklinlabs.com>
2025-02-14 09:56:33 -0500

    Make link signed.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,8 +1,16 @@
 class Screenshot < ApplicationRecord
   belongs_to :build
+  DEFAULT_EXPIRY = 3600
 
   def url
-    "https://#{ENV["DIGITALOCEAN_SPACES_BUCKET_NAME"]}.nyc3.digitaloceanspaces.com/#{path}"
+    signer = Aws::S3::Presigner.new(client: S3ClientFactory.client)
+
+    signer.presigned_url(
+      :get_object,
+      bucket: ENV["DIGITALOCEAN_SPACES_BUCKET_NAME"],
+      key: path,
+      expires_in: DEFAULT_EXPIRY
+    )
   end
 
   def label
