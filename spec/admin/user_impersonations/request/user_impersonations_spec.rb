38e6a8af16e0a6c5dec1084e896d0f883f9b59cf
Jason Swett <jason@benfranklinlabs.com>
2025-02-15 08:51:27 -0500

    Fix user impersonation bug.

--- spec/admin/user_impersonations/request/user_impersonations_spec.rb
+++ spec/admin/user_impersonations/request/user_impersonations_spec.rb
@@ -3,40 +3,50 @@ require "rails_helper"
 describe "User impersonations", type: :request do
   context "super admin" do
     let!(:super_admin_user) { create(:user, super_admin: true) }
-    let!(:other_user) { create(:user) }
-    let!(:other_user_project) { create(:project, user: other_user) }
+    let!(:target_user) { create(:user) }
 
     before do
       login_as(super_admin_user, scope: :user)
     end
 
-    it "redirects to the user's projects" do
-      post admin_user_impersonations_path(user_id: other_user.id)
-      expect(response).to redirect_to(other_user_project)
+    context "user has a project" do
+      let!(:target_user_project) { create(:project, user: target_user) }
+
+      it "redirects to the user's projects" do
+        post admin_user_impersonations_path(user_id: target_user.id)
+        expect(response).to redirect_to(target_user_project)
+      end
+
+      it "shows the target user's projects" do
+        post admin_user_impersonations_path(user_id: target_user.id)
+        get github_accounts_path
+        expect(response.body).to include("Signed in as #{target_user.name}")
+      end
     end
 
-    it "shows the other user's projects" do
-      post admin_user_impersonations_path(user_id: other_user.id)
-      get github_accounts_path
-      expect(response.body).to include("Signed in as #{other_user.name}")
+    context "user does not have a project" do
+      it "redirects somewhere else" do
+        post admin_user_impersonations_path(user_id: target_user.id)
+        expect(response.code).to eq("302")
+      end
     end
   end
 
   context "non-super admin" do
     let!(:non_super_admin_user) { create(:user) }
-    let!(:other_user) { create(:user) }
+    let!(:target_user) { create(:user) }
 
     before do
       login_as(non_super_admin_user, scope: :user)
     end
 
     it "returns a 404 response" do
-      post admin_user_impersonations_path(user_id: other_user.id)
+      post admin_user_impersonations_path(user_id: target_user.id)
       expect(response).to have_http_status(404)
     end
 
     it "does not render the page" do
-      post admin_user_impersonations_path(user_id: other_user.id)
+      post admin_user_impersonations_path(user_id: target_user.id)
       expect(response.body).to be_empty
     end
   end
