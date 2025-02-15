a24f6f794f58cb8e3e3ce5c83e8f93dd3ce47bcb
Jason Swett <jason@benfranklinlabs.com>
2025-02-14 22:11:32 -0500

    Fix controller.

--- app/controllers/api/v1/job_machine_images_controller.rb
+++ app/controllers/api/v1/job_machine_images_controller.rb
@@ -2,6 +2,7 @@ module API
   module V1
     class JobMachineImagesController < APIController
       def update
+        skip_authorization
         JobMachineImage.new(params[:id]).create_snapshot
         head :ok
       end
