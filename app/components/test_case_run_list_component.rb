f66c0ab63b6f9798558da3af158823708f69a62e
Jason Swett <jason@benfranklinlabs.com>
2025-02-16 12:45:20 -0500

    Squashed commit of the following:
    commit c7e7cf3669e52f8a92d03b174409b58118d48b63
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Feb 16 12:44:37 2025 -0500
    
        Refactoring.
    
    commit d171ff00daa75308722e23e2d9201d403ecd85c7
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Feb 16 12:39:31 2025 -0500
    
        Add component.

---
+++ app/components/test_case_run_list_component.rb
@@ -0,0 +1,20 @@
+class TestCaseRunListComponent < ViewComponent::Base
+  attr_reader :build, :active_test_case_run
+
+  def initialize(build:, active_test_case_run:)
+    @build = build
+    @active_test_case_run = active_test_case_run
+  end
+
+  def test_case_runs
+    TestCaseRun.failed_first(build.test_case_runs)
+  end
+
+  def test_case_run_count
+    build.test_case_runs.size
+  end
+
+  def failed_test_case_run_count
+    build.test_case_runs.select { |tcr| tcr.status == "failed" }.size
+  end
+end
