4383c8d60f961e22d7de3751fccdc438753d88b1
Jason Swett <jason@benfranklinlabs.com>
2025-02-16 10:47:00 -0500

    Add JS.

--- app/javascript/controllers/test_case_run_list_controller.js
+++ app/javascript/controllers/test_case_run_list_controller.js
@@ -3,6 +3,27 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["link"];
 
+  connect() {
+    this.element.addEventListener("scroll", this.onScroll.bind(this));
+  }
+
+  disconnect() {
+    this.element.removeEventListener("scroll", this.onScroll.bind(this));
+  }
+
+  onScroll() {
+    const list = this.element;
+    const buffer = 10;
+
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight - buffer) {
+      this.loadMore();
+    }
+  }
+
+  loadMore() {
+    // hit GET test_case_runs
+  }
+
   makeActive(event) {
     this.linkTargets.forEach(link => {
       link.classList.remove("active");
