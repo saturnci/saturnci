0c4fe8b65dbda964d965950f5fdc084452489d6e
Jason Swett <jason@benfranklinlabs.com>
2025-02-16 10:56:57 -0500

    Add quotes.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,14 +1,17 @@
 class Quote
   QUOTES = [
-    "\"Nothing is good or bad, but thinking makes it so\" - William Shakespeare",
-    "\"The hen is the wisest of all the animal creation, because she never cackles until the egg is laid.\" - Abraham Lincoln",
-    "\"Do what you can, with what you have, where you are.\" - Theodore Roosevelt",
-    "\"No man ever steps in the same river twice, for it's not the same river and he's not the same man.\" - Heraclitus",
     "\"An investment in knowledge pays the best interest.\" - Benjamin Franklin",
+    "\"Be a yardstick of quality. Some people aren't used to an environment where excellence is expected.\" - Steve Jobs",
+    "\"Design is not just what it looks like and feels like. Design is how it works.\" - Steve Jobs",
+    "\"Do what you can, with what you have, where you are.\" - Theodore Roosevelt",
     "\"Experience keeps a dear school, but fools will learn in no other.\" - Benjamin Franklin",
-    "\"The used key is always bright.\" - Benjamin Franklin",
-    "\"The recipe for great work is: very exacting taste, plus the ability to gratify it.\" - Paul Graham",
+    "\"Focus is about saying no.\" - Steve Jobs",
+    "\"No man ever steps in the same river twice, for it's not the same river and he's not the same man.\" - Heraclitus",
+    "\"Nothing is good or bad, but thinking makes it so.\" - William Shakespeare",
     "\"Opportunity is missed by most people because it is dressed in overalls and looks like work.\" - Thomas A. Edison",
+    "\"The hen is the wisest of all the animal creation, because she never cackles until the egg is laid.\" - Abraham Lincoln",
+    "\"The recipe for great work is: very exacting taste, plus the ability to gratify it.\" - Paul Graham",
+    "\"The used key is always bright.\" - Benjamin Franklin",
   ]
 
   def self.random
