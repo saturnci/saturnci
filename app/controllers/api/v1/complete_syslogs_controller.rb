32ea7929ce18d36373ac069b39fa2c917aa96171
Jason Swett <jason@benfranklinlabs.com>
2025-02-17 14:18:09 -0500

    Add rescue to CompleteSyslogsController.

--- app/controllers/api/v1/complete_syslogs_controller.rb
+++ app/controllers/api/v1/complete_syslogs_controller.rb
@@ -2,11 +2,16 @@ module API
   module V1
     class CompleteSyslogsController < APIController
       def create
-        run = Run.find(params[:run_id])
-        authorize run, :update?
+        begin
+          run = Run.find(params[:run_id])
+          authorize run, :update?
 
-        request.body.rewind
-        run.update!(complete_syslog: request.body.read)
+          request.body.rewind
+          run.update!(complete_syslog: request.body.read)
+        rescue StandardError => e
+          render(json: { error: e.message }, status: :bad_request)
+          return
+        end
 
         head :ok
       end
