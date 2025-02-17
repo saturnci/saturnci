99254ac00603c05ca4eed3ab7b1a669009046e91
Jason Swett <jason@benfranklinlabs.com>
2025-02-17 08:34:51 -0500

    Revert "Squashed commit of the following:"
    This reverts commit a6601d57b4188946b71b7414a65149d1cfbc8e32.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   resources :builds, only: [] do
-    resources :runs, only: :create
     resources :test_case_runs, only: :index
   end
 
