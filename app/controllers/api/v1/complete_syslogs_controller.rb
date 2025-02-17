b3cb914ef925ffae10e2a739b915634b25441973
Jason Swett <jason@benfranklinlabs.com>
2025-02-17 14:03:28 -0500

    Fix controller.

--- app/controllers/api/v1/complete_syslogs_controller.rb
+++ app/controllers/api/v1/complete_syslogs_controller.rb
@@ -3,6 +3,8 @@ module API
     class CompleteSyslogsController < APIController
       def create
         run = Run.find(params[:run_id])
+        authorize run, :update?
+
         request.body.rewind
         run.update!(complete_syslog: request.body.read)
 
