bb0078e293698ff5a3676dde005c29e919ab402c
Jason Swett <jason@benfranklinlabs.com>
2025-02-19 08:40:08 -0500

    Add ability to destroy all droplets.

---
+++ app/models/runner.rb
@@ -0,0 +1,11 @@
+class Runner
+  def self.destroy_all
+    client = DropletKitClientFactory.client
+
+    droplets = client.droplets.all(tag_name: "saturnci")
+
+    droplets.each do |droplet|
+      client.droplets.delete(id: droplet.id)
+    end
+  end
+end
