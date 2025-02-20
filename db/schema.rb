32cdb63733367d5aee1803006a10ebbd9ff8dcc5
Jason Swett <jason@benfranklinlabs.com>
2025-02-20 12:31:01 -0500

    Drop runs.system_logs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_19_181113) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_20_172006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -119,7 +119,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_19_181113) do
     t.string "runner_id"
     t.text "test_output"
     t.text "test_report"
-    t.text "system_logs"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order_index", null: false
