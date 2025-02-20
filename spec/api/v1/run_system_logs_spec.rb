75080dc6f99396522fdcbdcf7af43901df113afe
Jason Swett <jason@benfranklinlabs.com>
2025-02-20 12:19:49 -0500

    De-duplicate test.

--- spec/api/v1/run_system_logs_spec.rb
+++ spec/api/v1/run_system_logs_spec.rb
@@ -16,17 +16,6 @@ describe "system logs", type: :request do
       expect(run.reload.system_logs).to eq("system log content")
       expect(response.status).to eq(200)
     end
-
-    it "adds system logs to a runner system log" do
-      post(
-        api_v1_run_system_logs_path(run_id: run.id, format: :json),
-        params: Base64.encode64("system log content"),
-        headers: api_authorization_headers(user).merge({ "CONTENT_TYPE" => "text/plain" })
-      )
-
-      expect(run.runner_system_log.reload.content).to eq("system log content")
-      expect(response.status).to eq(200)
-    end
   end
 
   describe "appending" do
