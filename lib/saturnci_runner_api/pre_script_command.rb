587d2c0186ec622cc904b9426d1734c163c36277
Jason Swett <jason@benfranklinlabs.com>
2025-02-21 17:34:40 -0500

    Simplify pre command.

--- lib/saturnci_runner_api/pre_script_command.rb
+++ lib/saturnci_runner_api/pre_script_command.rb
@@ -5,7 +5,7 @@ module SaturnCIRunnerAPI
     end
 
     def to_s
-      "script -c \"sudo #{@docker_compose_configuration.script_env_vars} docker exec saturn_test_app ./.saturnci/pre.sh\""
+      "script -c \"sudo docker-compose exec saturn_test_app ./.saturnci/pre.sh\""
     end
   end
 end
