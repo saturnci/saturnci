591fb8f95c9d8f705d1f1b434dfdab6e492367c2
Jason Swett <jason@benfranklinlabs.com>
2025-02-24 08:49:05 -0500

    Squashed commit of the following:
    commit aa5b229523ccc8afb2bd98e9602f7da620a26aff
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Feb 23 22:51:01 2025 -0500
    
        Rename BuildComponent to TestSuiteRunComponent.

--- app/controllers/test_case_runs_controller.rb
+++ app/controllers/test_case_runs_controller.rb
@@ -31,7 +31,7 @@ class TestCaseRunsController < ApplicationController
 
     @build = @test_case_run.run.build
 
-    @build_component = BuildComponent.new(
+    @test_suite_run_component = TestSuiteRunComponent.new(
       build: @build,
       current_tab_name: "overview",
       branch_name: params[:branch_name],
