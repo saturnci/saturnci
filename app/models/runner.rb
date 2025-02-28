d8df2bf80b382b92ffeb20f61a9288f891b8289e
Jason Swett <jason@benfranklinlabs.com>
2025-02-28 15:37:06 -0500

    Add destroy_orphaned.

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,7 +1,6 @@
 class Runner
   def self.destroy_all
     client = DropletKitClientFactory.client
-
     droplets = client.droplets.all(tag_name: "saturnci")
 
     droplets.each do |droplet|
@@ -10,5 +9,15 @@ class Runner
   end
 
   def self.destroy_orphaned
+    active_runner_ids = Run.running.map(&:runner_id)
+
+    client = DropletKitClientFactory.client
+    droplets = client.droplets.all(tag_name: "saturnci")
+
+    droplets.reject do |droplet|
+      droplet.id.in?(active_runner_ids)
+    end.each do |droplet|
+      client.droplets.delete(id: droplet.id)
+    end
   end
 end
