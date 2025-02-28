d2d9b60470d8a1e49314d18d6dc311f1f8a4f67c
Jason Swett <jason@benfranklinlabs.com>
2025-02-27 20:27:06 -0500

    Change test labels.

---
+++ spec/factories/test_suite_runs.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :test_suite_run do
+    project
+    branch_name { "main" }
+    author_name { Faker::Name.name }
+    commit_hash { Faker::Alphanumeric.alphanumeric(number: 7) }
+    commit_message { "Make change." }
+
+    trait :with_run do
+      after(:create) do |test_suite_run|
+        create(:run, test_suite_run: test_suite_run)
+      end
+    end
+
+    trait :with_passed_run do
+      after(:create) do |test_suite_run|
+        create(:run, :passed, test_suite_run: test_suite_run)
+      end
+    end
+
+    trait :with_failed_run do
+      after(:create) do |test_suite_run|
+        create(:run, :failed, test_suite_run: test_suite_run)
+      end
+    end
+  end
+end
