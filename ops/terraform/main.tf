6d9d5a4b384f3165f1d748e9c9464199f66f8c45
Jason Swett <jason@benfranklinlabs.com>
2025-03-01 14:28:11 -0500

    Add cache db.

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -20,9 +20,23 @@ resource "digitalocean_database_cluster" "postgres" {
   node_count = 1
 }
 
+resource "digitalocean_database_cluster" "postgres_cache" {
+  name       = "saturnci-production-db-cache"
+  engine     = "pg"
+  version    = "15"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
+
 variable "do_token" {}
 
 output "db_uri" {
   value = digitalocean_database_cluster.postgres.uri
+  sensitive = true
+}
+
+output "cache_db_uri" {
+  value = digitalocean_database_cluster.postgres_cache.uri
   sensitive = true
 }
