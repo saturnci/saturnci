b32e7fb9811b775d9ddd3c79f08fbe0ba0cf7ef5
Jason Swett <jason@benfranklinlabs.com>
2025-03-02 09:16:39 -0500

    Rename instances of build to test suite run.

--- spec/test_suite_runs/build_status/models/test_suite_status_spec.rb
+++ spec/test_suite_runs/build_status/models/test_suite_status_spec.rb
@@ -1,26 +1,26 @@
 require "rails_helper"
 
-RSpec.describe "build status", type: :model do
+describe "test suite run status", type: :model do
   describe "#status" do
     context "no runs" do
       it "is 'Not Started'" do
-        build = create(:build)
-        expect(build.status).to eq("Not Started")
+        test_suite_run = create(:build)
+        expect(test_suite_run.status).to eq("Not Started")
       end
     end
 
     context "some runs" do
-      let!(:build) { create(:build) }
-      let!(:run_1) { create(:run, build: build, order_index: 1) }
-      let!(:run_2) { create(:run, build: build, order_index: 2) }
+      let!(:test_suite_run) { create(:build) }
+      let!(:run_1) { create(:run, build: test_suite_run, order_index: 1) }
+      let!(:run_2) { create(:run, build: test_suite_run, order_index: 2) }
 
       context "all runs have passed" do
         it "is passed" do
           allow(run_1).to receive(:status).and_return("Passed")
           allow(run_2).to receive(:status).and_return("Passed")
-          allow(build).to receive(:runs).and_return([run_1, run_2])
+          allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
 
-          expect(build.status).to eq("Passed")
+          expect(test_suite_run.status).to eq("Passed")
         end
       end
 
@@ -28,9 +28,9 @@ RSpec.describe "build status", type: :model do
         it "is failed" do
           allow(run_1).to receive(:status).and_return("Passed")
           allow(run_2).to receive(:status).and_return("Failed")
-          allow(build).to receive(:runs).and_return([run_1, run_2])
+          allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
 
-          expect(build.status).to eq("Failed")
+          expect(test_suite_run.status).to eq("Failed")
         end
       end
 
@@ -38,17 +38,17 @@ RSpec.describe "build status", type: :model do
         it "is running" do
           allow(run_1).to receive(:status).and_return("Passed")
           allow(run_2).to receive(:status).and_return("Running")
-          allow(build).to receive(:runs).and_return([run_1, run_2])
+          allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
 
-          expect(build.status).to eq("Running")
+          expect(test_suite_run.status).to eq("Running")
         end
       end
 
       context "there are no runs" do
         it "is not started" do
-          allow(build).to receive(:runs).and_return([])
+          allow(test_suite_run).to receive(:runs).and_return([])
 
-          expect(build.status).to eq("Not Started")
+          expect(test_suite_run.status).to eq("Not Started")
         end
       end
 
@@ -57,40 +57,40 @@ RSpec.describe "build status", type: :model do
           before do
             allow(run_1).to receive(:status).and_return("Passed")
             allow(run_2).to receive(:status).and_return("Passed")
-            allow(build).to receive(:runs).and_return([run_1, run_2])
+            allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
           end
 
           it "sets the cached status" do
-            expect { build.status }.to change { build.reload.cached_status }
+            expect { test_suite_run.status }.to change { test_suite_run.reload.cached_status }
               .from(nil).to("Passed")
           end
         end
 
         context "cached_status does not match calculated_status" do
           before do
-            build.update!(cached_status: "Passed")
+            test_suite_run.update!(cached_status: "Passed")
             allow(run_1).to receive(:status).and_return("Failed")
             allow(run_2).to receive(:status).and_return("Failed")
-            allow(build).to receive(:runs).and_return([run_1, run_2])
+            allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
           end
 
           it "sets the cached status" do
-            expect { build.status }.to change { build.reload.cached_status }
+            expect { test_suite_run.status }.to change { test_suite_run.reload.cached_status }
               .from("Passed").to("Failed")
           end
         end
 
         context "cached_status matches calculated_status" do
           before do
-            build.update!(cached_status: "Passed")
+            test_suite_run.update!(cached_status: "Passed")
             allow(run_1).to receive(:status).and_return("Passed")
             allow(run_2).to receive(:status).and_return("Passed")
-            allow(build).to receive(:runs).and_return([run_1, run_2])
+            allow(test_suite_run).to receive(:runs).and_return([run_1, run_2])
           end
 
-          it "does not update the build" do
-            expect(build).not_to receive(:update!)
-            build.status
+          it "does not update the test suite run" do
+            expect(test_suite_run).not_to receive(:update!)
+            test_suite_run.status
           end
         end
       end
