0f5ac3b052aff8d7cd8c7c0d08f89834d6268b47
Jason Swett <jason@benfranklinlabs.com>
2025-03-03 14:39:16 -0500

    Add queue db in Terraform.

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -29,14 +29,28 @@ resource "digitalocean_database_cluster" "postgres_cache" {
   node_count = 1
 }
 
+resource "digitalocean_database_cluster" "postgres_queue" {
+  name       = "saturnci-production-db-queue"
+  engine     = "pg"
+  version    = "15"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
+
 variable "do_token" {}
 
 output "db_uri" {
-  value = digitalocean_database_cluster.postgres.uri
+  value     = digitalocean_database_cluster.postgres.uri
   sensitive = true
 }
 
 output "cache_db_uri" {
-  value = digitalocean_database_cluster.postgres_cache.uri
+  value     = digitalocean_database_cluster.postgres_cache.uri
+  sensitive = true
+}
+
+output "queue_db_uri" {
+  value     = digitalocean_database_cluster.postgres_queue.uri
   sensitive = true
 }
