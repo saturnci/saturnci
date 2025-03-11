6c4e5be1bb85d9d415ab183e1cbbc1224dea49be
Jason Swett <jason@benfranklinlabs.com>
2025-03-11 08:17:36 -0400

    Add rsa_keys.test_runner_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_113017) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_121636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -86,6 +86,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_113017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "run_id"
+    t.uuid "test_runner_id"
+    t.index ["test_runner_id"], name: "index_rsa_keys_on_test_runner_id"
   end
 
   create_table "run_events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -226,6 +228,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_113017) do
   add_foreign_key "project_secrets", "projects"
   add_foreign_key "projects", "github_accounts"
   add_foreign_key "projects", "users"
+  add_foreign_key "rsa_keys", "test_runners"
   add_foreign_key "run_events", "runs"
   add_foreign_key "run_test_runners", "runs"
   add_foreign_key "run_test_runners", "test_runners"
