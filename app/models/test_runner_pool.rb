49f813989c96fda9805ff7c00f408381de66bd52
Jason Swett <jason@benfranklinlabs.com>
2025-03-11 10:57:59 -0400

    Add script.

--- app/models/test_runner_pool.rb
+++ app/models/test_runner_pool.rb
@@ -4,8 +4,14 @@ class TestRunnerPool
       TestRunner.destroy_all
 
       count.times do
-        TestRunner.provision(client:)
+        TestRunner.provision(client:, user_data: script)
       end
     end
+  end
+
+  def self.script
+    <<~SCRIPT
+      git clone git@github.com:saturnci/test_runner_agent.git
+    SCRIPT
   end
 end
