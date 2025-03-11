16ae70f71d00684bd54d55d236c15e51c98193bf
Jason Swett <jason@benfranklinlabs.com>
2025-03-11 08:51:18 -0400

    Remove rsa_keys.run_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_124556) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_125040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -85,7 +85,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_124556) do
     t.text "private_key_value", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "run_id"
   end
 
   create_table "run_events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
