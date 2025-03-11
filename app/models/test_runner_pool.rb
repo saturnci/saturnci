78abc573602bb3426de9e9a4aef963c3892f3d51
Jason Swett <jason@benfranklinlabs.com>
2025-03-11 07:18:19 -0400

    Improve scaling.

--- app/models/test_runner_pool.rb
+++ app/models/test_runner_pool.rb
@@ -1,10 +1,14 @@
 class TestRunnerPool
   def self.scale(count)
-    10.times do
-      TestRunner.create!(
-        name: SecureRandom.hex(8),
-        cloud_id: SecureRandom.hex(8)
-      )
+    ActiveRecord::Base.transaction do
+      TestRunner.destroy_all
+
+      count.times do
+        TestRunner.create!(
+          name: SecureRandom.hex(8),
+          cloud_id: SecureRandom.hex(8)
+        )
+      end
     end
   end
 end
--- spec/models/test_runner_pool_spec.rb
+++ spec/models/test_runner_pool_spec.rb
@@ -2,10 +2,29 @@ require "rails_helper"
 
 describe TestRunnerPool do
   describe "#scale" do
-    it "works" do
-      expect { TestRunnerPool.scale(10) }
-        .to change { TestRunner.count }
-        .from(0).to(10)
+    context "scaling to 10" do
+      it "creates 10 test runners" do
+        expect { TestRunnerPool.scale(10) }
+          .to change { TestRunner.count }
+          .from(0).to(10)
+      end
+    end
+
+    context "scaling to 2" do
+      it "creates 2 test runners" do
+        expect { TestRunnerPool.scale(2) }
+          .to change { TestRunner.count }
+          .from(0).to(2)
+      end
+    end
+
+    context "scaling up and then back down" do
+      it "works" do
+        TestRunnerPool.scale(10)
+        expect { TestRunnerPool.scale(2) }
+          .to change { TestRunner.count }
+          .from(10).to(2)
+      end
     end
   end
 end
