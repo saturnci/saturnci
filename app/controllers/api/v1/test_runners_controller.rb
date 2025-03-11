0733ac1cc33f20c0a274b29dd192a1ec6056f236
Jason Swett <jason@benfranklinlabs.com>
2025-03-11 16:19:41 -0400

    Add sorting.

--- app/controllers/api/v1/test_runners_controller.rb
+++ app/controllers/api/v1/test_runners_controller.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class TestRunnersController < APIController
       def index
-        test_runners = TestRunner.all
+        test_runners = TestRunner.order("created_at desc")
         authorize test_runners
         render json: test_runners
       rescue StandardError => e
