5f1643dba589584095a1e914fc77e930877867b4
Jason Swett <jason@benfranklinlabs.com>
2025-03-12 17:35:06 -0400

    Remove auth.

--- app/controllers/silliness_controller.rb
+++ app/controllers/silliness_controller.rb
@@ -1,4 +1,7 @@
 class SillinessController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user_or_404!
+
   def index
     skip_authorization
     render plain: SillyName.random
