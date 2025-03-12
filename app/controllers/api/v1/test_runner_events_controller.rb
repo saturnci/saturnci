2421b74ddc29913f7b9e7eff29dc2037944dce00
Jason Swett <jason@benfranklinlabs.com>
2025-03-12 11:42:32 -0400

    Get controller working.

--- app/controllers/api/v1/test_runner_events_controller.rb
+++ app/controllers/api/v1/test_runner_events_controller.rb
@@ -2,6 +2,8 @@ module API
   module V1
     class TestRunnerEventsController < APIController
       def create
+        test_runner = TestRunner.find(params[:test_runner_id])
+        test_runner.test_runner_events.create!(type: params[:type])
       rescue StandardError => e
         render(json: { error: e.message }, status: :bad_request)
       end
