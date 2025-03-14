17d296f9453694470999837f391b61229f237192
Jason Swett <jason@benfranklinlabs.com>
2025-03-14 08:12:42 -0400

    Simplify debug output.

--- app/jobs/assign_test_runners_job.rb
+++ app/jobs/assign_test_runners_job.rb
@@ -2,33 +2,40 @@ class AssignTestRunnersJob < ApplicationJob
   queue_as :default
 
   def perform(run_ids)
-    logger.info "[AssignTestRunnersJob] Started with run_ids: #{run_ids.inspect}"
+    logger.info "Started with run_ids: #{run_ids.inspect}"
+
     runs = Run.find(run_ids)
-    logger.info "[AssignTestRunnersJob] Found #{runs.size} runs to process"
+    logger.info "Found #{runs.size} runs to process"
 
     while runs.any?
-      logger.info "[AssignTestRunnersJob] Remaining runs: #{runs.size}"
+      logger.info "Remaining runs: #{runs.size}"
+
       available_test_runners = nil
       loop do
         ActiveRecord::Base.uncached do
           available_test_runners = TestRunner.available.to_a
         end
 
-        logger.info "[AssignTestRunnersJob] Available test runners: #{available_test_runners.size}"
+        logger.info "Available test runners: #{available_test_runners.size}"
+
         if available_test_runners.any?
           break
         else
-          logger.info "[AssignTestRunnersJob] No test runners available, waiting..."
+          logger.info "No test runners available, waiting..."
           sleep(rand(1..5)) # avoid hammering the database
           sleep(Rails.configuration.test_runner_availability_check_interval_in_seconds)
         end
       end
+
       run = runs.shift
-      logger.info "[AssignTestRunnersJob] Processing run: #{run.id}"
+      logger.info "Processing run: #{run.id}"
+
       test_runner = available_test_runners.shift
-      logger.info "[AssignTestRunnersJob] Assigning test runner: #{test_runner.id} to run: #{run.id}"
+
+      logger.info "Assigning test runner: #{test_runner.id} to run: #{run.id}"
       test_runner.assign(run)
     end
-    logger.info "[AssignTestRunnersJob] Completed"
+
+    logger.info "Completed"
   end
 end
