e2f8260526551ca193e39aebc58c2a6c1f82afbd
Jason Swett <jason@benfranklinlabs.com>
2025-03-18 09:55:37 -0400

    Fix test.

--- spec/test_suite_runs/start_spec.rb
+++ spec/test_suite_runs/start_spec.rb
@@ -4,45 +4,17 @@ describe "Starting test suite run" do
   let!(:project) { create(:project, concurrency: 2) }
   let!(:test_suite_run) { create(:build, project:) }
 
-  around do |example|
-    perform_enqueued_jobs do
-      example.run
-    end
-  end
-
-  context "there were initially no test runners, then some became available" do
-    let!(:test_runners) do
-      create_list(:test_runner, 2)
-    end
-
+  context "there are available test runners" do
     before do
-      allow(TestRunner).to receive(:provision)
-
-      allow(TestRunner).to receive_message_chain(:available, :count)
-        .and_return(0)
-
-      allow(TestRunner).to receive_message_chain(:available, :to_a)
-        .and_return([], test_runners)
+      allow(TestRunner).to receive(:available).and_return(create_list(:test_runner, 2))
+      allow(TestRunner).to receive(:create_vm)
     end
 
     it "makes the assignments" do
-      expect { test_suite_run.start! }
-        .to change(TestRunnerAssignment, :count).by(2)
-    end
-  end
-
-  context "there are no available test runners" do
-    before do
-      allow(TestRunner).to receive(:provision) do
-        create(:test_runner) do |test_runner|
-          test_runner.test_runner_events.create!(type: :ready_signal_received)
-        end
-      end
-    end
-
-    it "makes the assignments anyway" do
-      expect { test_suite_run.start! }
-        .to change(TestRunnerAssignment, :count).by(2)
+      expect do
+        test_suite_run.start!
+        TestRunner.supervise
+      end.to change(TestRunnerAssignment, :count).by(2)
     end
   end
 
