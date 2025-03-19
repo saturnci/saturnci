484499c36b6c0b9e9e1e677b7839521edcdc867f
Jason Swett <jason@benfranklinlabs.com>
2025-03-19 11:24:45 -0400

    Fix logging to work in dev.

--- app/models/test_runner_supervisor.rb
+++ app/models/test_runner_supervisor.rb
@@ -1,6 +1,6 @@
 class TestRunnerSupervisor
   def self.check
-    Rails.logger.info "-" * 80
+    log "-" * 80
 
     remove_orphaned_test_runner_assignments
 
@@ -11,46 +11,46 @@ class TestRunnerSupervisor
     # if those tests' test runner assignments have been deleted
     unassigned_runs = Run.unassigned.where("runs.created_at > ?", 1.day.ago)
 
-    Rails.logger.info "Unassigned runs: #{unassigned_runs.count}"
+    log "Unassigned runs: #{unassigned_runs.count}"
 
     ActiveRecord::Base.uncached do
       available_test_runners = TestRunner.available.to_a.shuffle
       unassigned_test_runners = TestRunner.unassigned
     end
-    Rails.logger.info "Available test runners: #{available_test_runners.count}"
-    Rails.logger.info "Unassigned test runners: #{unassigned_test_runners.count}"
+    log "Available test runners: #{available_test_runners.count}"
+    log "Unassigned test runners: #{unassigned_test_runners.count}"
 
     if unassigned_test_runners.count < unassigned_runs.count
       number_of_needed_test_runners = unassigned_runs.count - unassigned_test_runners.count
-      Rails.logger.info "Provisioning #{number_of_needed_test_runners} test runners"
+      log "Provisioning #{number_of_needed_test_runners} test runners"
       number_of_needed_test_runners.times { TestRunner.provision }
     end
 
     unassigned_runs.each do |run|
       break if available_test_runners.empty?
       test_runner = available_test_runners.shift
-      Rails.logger.info "Assigning #{test_runner.name} to #{run.id}"
+      log "Assigning #{test_runner.name} to #{run.id}"
       test_runner.assign(run)
     end
 
     test_runner_pool_size = ENV.fetch("TEST_RUNNER_POOL_SIZE", 10).to_i
-    Rails.logger.info "Test runner pool size: #{test_runner_pool_size}"
+    log "Test runner pool size: #{test_runner_pool_size}"
 
     if unassigned_test_runners.count < test_runner_pool_size
       number_of_needed_test_runners = test_runner_pool_size - unassigned_test_runners.count
-      Rails.logger.info "Provisioning #{number_of_needed_test_runners} test runners"
+      log "Provisioning #{number_of_needed_test_runners} test runners"
       number_of_needed_test_runners.times { TestRunner.provision }
     end
   end
 
   def self.remove_orphaned_test_runner_assignments
-    Rails.logger.info "Deleting #{orphaned_test_runner_assignments.count} orphaned test runners"
+    log "Deleting #{orphaned_test_runner_assignments.count} orphaned test runners"
 
     ActiveRecord::Base.transaction do
       orphaned_test_runner_assignments.each do |test_runner_assignment|
         test_runner_assignment.test_runner.test_runner_events.create!(type: :error)
-        Rails.logger.info "Deleting orphaned test runner assignment: #{test_runner_assignment.id}"
-        Rails.logger.info "Test runner: #{test_runner_assignment.test_runner.name}"
+        log "Deleting orphaned test runner assignment: #{test_runner_assignment.id}"
+        log "Test runner: #{test_runner_assignment.test_runner.name}"
         test_runner_assignment.destroy
       end
     end
@@ -62,5 +62,13 @@ class TestRunnerSupervisor
     TestRunnerAssignment
       .where("test_runner_assignments.created_at > ?", 1.day.ago)
       .where.not(test_runner_id: non_orphaned_test_runners)
+  end
+
+  def self.log(message)
+    if Rails.env.production?
+      Rails.logger.info message
+    else
+      puts message
+    end
   end
 end
