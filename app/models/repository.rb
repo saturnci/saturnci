e34ce50ab2853ca14690bb6bab79ad8809d75d2d
Jason Swett <jason@benfranklinlabs.com>
2025-04-03 08:45:20 -0400

    Fix other association issue.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,7 +2,7 @@ class Repository < ApplicationRecord
   acts_as_paranoid
   has_many :builds
   has_many :test_suite_runs, dependent: :destroy, foreign_key: "project_id"
-  has_many :runs, through: :builds
+  has_many :runs, through: :test_suite_runs
   has_many :project_secrets, foreign_key: "project_id"
   belongs_to :user
   belongs_to :github_account
