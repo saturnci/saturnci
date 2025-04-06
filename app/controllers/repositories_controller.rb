47b08a097ad87468ab6829ed7e3f3ce8860e8444
Jason Swett <jason@benfranklinlabs.com>
2025-04-06 09:14:07 -0400

    Fix bug.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,18 @@
 class RepositoriesController < ApplicationController
+  skip_before_action :authenticate_user_or_404!, only: :index
+
   def index
-    @repositories = current_user.repositories
-    authorize @repositories
+    if user_signed_in?
+      if current_user.email.present?
+        @repositories = current_user.repositories
+        authorize @repositories
+      else
+        skip_authorization
+        redirect_to new_user_email_path
+      end
+    else
+      skip_authorization
+      redirect_to new_user_session_path
+    end
   end
 end
