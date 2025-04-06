b41ad8597ea41a3b31e8ea0047f9377f6f7f6170
Jason Swett <jason@benfranklinlabs.com>
2025-04-06 09:46:17 -0400

    Fix TestSuiteRunLink misnomer.

--- app/models/test_suite_run_link_path.rb
+++ app/models/test_suite_run_link_path.rb
@@ -2,16 +2,16 @@ class TestSuiteRunLinkPath
   include Rails.application.routes.url_helpers
   DEFAULT_PARTIAL = "test_output"
 
-  def initialize(build)
-    @build = build
+  def initialize(test_suite_run)
+    @test_suite_run = test_suite_run
   end
 
   def value
     Rails.cache.fetch(cache_key) do
-      if @build.finished? || @build.runs.empty?
-        project_build_path(@build.project, @build)
+      if @test_suite_run.finished? || @test_suite_run.runs.empty?
+        project_test_suite_run_path(@test_suite_run.project, @test_suite_run)
       else
-        run_path(first_failed_run || @build.runs.sorted.first, DEFAULT_PARTIAL)
+        run_path(first_failed_run || @test_suite_run.runs.sorted.first, DEFAULT_PARTIAL)
       end
     end
   end
@@ -19,14 +19,14 @@ class TestSuiteRunLinkPath
   private
 
   def first_failed_run
-    @build.runs.select(&:failed?).first
+    @test_suite_run.runs.select(&:failed?).first
   end
 
   def cache_key
     [
       "test_suite_run_link_path",
-      @build.id,
-      @build.updated_at.to_i
+      @test_suite_run.id,
+      @test_suite_run.updated_at.to_i
     ].join("/")
   end
 end
