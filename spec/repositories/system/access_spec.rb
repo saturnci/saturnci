91b8ba6ff694ec7724dcdfe61116d150805c82ce
Jason Swett <jason@benfranklinlabs.com>
2025-04-28 09:02:53 -0400

    Add negative test case.

--- spec/repositories/system/access_spec.rb
+++ spec/repositories/system/access_spec.rb
@@ -7,21 +7,38 @@ describe "Repository access", type: :system do
   before do
     create(:repository, name: "panda", github_repo_full_name: "panda")
 
-    allow(github_client).to receive(:repositories).and_return([
-      double(full_name: "panda")
-    ])
-
     allow(github_client).to receive(:last_response).and_return(
       double(rels: { next: nil })
     )
-
-    allow_any_instance_of(User).to receive(:github_client).and_return(github_client)
-
-    login_as(user)
   end
 
-  it "works" do
-    visit repositories_path
-    expect(page).to have_content("panda")
+  context "user has access" do
+    before do
+      allow(github_client).to receive(:repositories).and_return([
+        double(full_name: "panda")
+      ])
+
+      allow_any_instance_of(User).to receive(:github_client).and_return(github_client)
+      login_as(user)
+    end
+
+    it "shows the repository" do
+      visit repositories_path
+      expect(page).to have_content("panda")
+    end
+  end
+
+  context "user does not have access" do
+    before do
+      allow(github_client).to receive(:repositories).and_return([])
+
+      allow_any_instance_of(User).to receive(:github_client).and_return(github_client)
+      login_as(user)
+    end
+
+    it "does not show the repository" do
+      visit repositories_path
+      expect(page).not_to have_content("panda")
+    end
   end
 end
