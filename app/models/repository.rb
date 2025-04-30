7ab1a25e98f792cad3c8e53f75fdc9b033b7423e
Jason Swett <jason@benfranklinlabs.com>
2025-04-30 08:28:17 -0400

    Remove user association from Repository.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,7 +4,6 @@ class Repository < ApplicationRecord
   has_many :test_suite_runs, dependent: :destroy, foreign_key: "project_id"
   has_many :runs, through: :test_suite_runs
   has_many :project_secrets, foreign_key: "project_id"
-  belongs_to :user
   belongs_to :github_account
 
   def to_s
