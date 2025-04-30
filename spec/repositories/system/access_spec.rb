035e0a527d82bced37b05a4c32dc1adb557c31db
Jason Swett <jason@codewithjason.com>
2025-04-30 08:23:29 -0400

    Reconcile repositories against GitHub (#36)
    * Reconcile repos against GitHub.
    
    * Add negative test case.

--- spec/repositories/system/access_spec.rb
+++ spec/repositories/system/access_spec.rb
@@ -2,19 +2,43 @@ require "rails_helper"
 
 describe "Repository access", type: :system do
   let!(:user) { create(:user) }
+  let!(:github_client) { instance_double(Octokit::Client) }
 
   before do
-    login_as(user)
+    create(:repository, name: "panda", github_repo_full_name: "panda")
+
+    allow(github_client).to receive(:last_response).and_return(
+      double(rels: { next: nil })
+    )
   end
 
-  xit "works" do
-    # before:
-    # there's a repo called panda
-    # user has GitHub access to panda
-    # panda exists as a repository in SaturnCI
-    create(:repository, name: "panda")
+  context "user has access" do
+    before do
+      allow(github_client).to receive(:repositories).and_return([
+        double(full_name: "panda")
+      ])
 
-    visit repositories_path
-    expect(page).to have_content("panda")
+      allow_any_instance_of(User).to receive(:github_client).and_return(github_client)
+      login_as(user)
+    end
+
+    it "shows the repository" do
+      visit repositories_path
+      expect(page).to have_content("panda")
+    end
+  end
+
+  context "user does not have access" do
+    before do
+      allow(github_client).to receive(:repositories).and_return([])
+
+      allow_any_instance_of(User).to receive(:github_client).and_return(github_client)
+      login_as(user)
+    end
+
+    it "does not show the repository" do
+      visit repositories_path
+      expect(page).not_to have_content("panda")
+    end
   end
 end
