7c8e2baef3bef8219e5809d014536188756fcf5d
Jason Swett <jason@benfranklinlabs.com>
2025-04-30 08:54:11 -0400

    Fix bug.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ require "octokit"
 
 class User < ApplicationRecord
   acts_as_paranoid
-  has_many :repositories
   has_many :projects
   has_many :runs, through: :projects
   has_many :github_accounts
