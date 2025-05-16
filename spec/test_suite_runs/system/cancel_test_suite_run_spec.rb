da64df37dd764a1c420f2869398e4b136e3d847c
Jason Swett <jason@benfranklinlabs.com>
2025-05-16 12:47:43 -0400

    Fix tests.

--- spec/test_suite_runs/system/cancel_test_suite_run_spec.rb
+++ spec/test_suite_runs/system/cancel_test_suite_run_spec.rb
@@ -5,7 +5,9 @@ describe "Cancel test suite run", type: :system do
 
   before do
     stub_request(:delete, "https://api.digitalocean.com/v2/droplets/#{run.test_runner.cloud_id}").to_return(status: 200)
-    allow_any_instance_of(User).to receive(:github_repositories).and_return([run.test_suite_run.repository])
+
+    allow_any_instance_of(User).to receive(:can_hit_github_api?).and_return(true)
+    allow_any_instance_of(User).to receive(:can_access_repository?).and_return(true)
     login_as(run.test_suite_run.repository.user)
   end
 
