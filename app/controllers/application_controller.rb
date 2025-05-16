1468d575bbb36075192c6902b1bc527ce15c651f
Jason Swett <jason@benfranklinlabs.com>
2025-05-16 11:35:03 -0400

    Revert "Fix method name."
    This reverts commit 1b2e47bc89411bbffddb024fcbe749ce41026084.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :authenticate_user_or_404!, unless: :devise_controller?
-  before_action :check_github_api_access, if: :user_signed_in?
+  before_action :can_hit_github_api?, if: :user_signed_in?
   after_action :verify_authorized, unless: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     head :not_found
   end
 
-  def check_github_api_access
+  def can_hit_github_api?
     begin
       current_user.github_client.user
     rescue Octokit::Unauthorized
