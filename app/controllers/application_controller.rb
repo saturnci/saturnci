4ed35c4545ea87dc0e3fb6006d73cdf79c2a4551
Jason Swett <jason@benfranklinlabs.com>
2025-05-16 12:10:07 -0400

    Revert "Revert "Make GitHub API check universal.""
    This reverts commit 3abb1630d19b265c150dfccf4ef6dd39b305e891.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :authenticate_user_or_404!, unless: :devise_controller?
+  before_action :can_hit_github_api?, if: :user_signed_in?
   after_action :verify_authorized, unless: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -14,5 +15,14 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     head :not_found
+  end
+
+  def can_hit_github_api?
+    begin
+      current_user.github_client.user
+    rescue Octokit::Unauthorized
+      sign_out current_user
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/users/github_api.rb
+++ app/models/users/github_api.rb
@@ -22,10 +22,12 @@ module Users
     def github_repositories
       Rails.cache.fetch("user/#{id}/github_repositories", expires_in: 1.week) do
         repositories = github_client.repositories
+
         loop do
           break if github_client.last_response.rels[:next].nil?
           repositories.concat github_client.get(github_client.last_response.rels[:next].href)
         end
+
         Repository.where(github_repo_full_name: repositories.map(&:full_name))
       end
     end
