eebb7f93df3f954d515ce5ae8e5022dd342f856e
Jason Swett <jason@benfranklinlabs.com>
2025-05-21 06:16:35 -0400

    Fix user script.

--- app/models/test_runner_snapshot.rb
+++ app/models/test_runner_snapshot.rb
@@ -51,7 +51,7 @@ class TestRunnerSnapshot < ApplicationRecord
       apt-get update
       apt-get install -y apt-transport-https ca-certificates curl software-properties-common
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-      add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
       apt-get update
       apt-get install -y docker-ce
 
