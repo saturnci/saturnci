b25009c0015d988bed6951913e9a8538a82b15b7
Jason Swett <jason@benfranklinlabs.com>
2025-05-22 16:44:13 -0400

    Allow super admin to edit secrets.

--- app/policies/project_secret_collection_policy.rb
+++ app/policies/project_secret_collection_policy.rb
@@ -1,9 +1,11 @@
 class ProjectSecretCollectionPolicy < ApplicationPolicy
   def show?
+    return true if user.super_admin?
     record.project_secrets.map(&:project).compact.all? { |project| project.user == user }
   end
 
   def update?
+    return true if user.super_admin?
     record.repository.user == user
   end
 end
