ecd6334750f429c3e243f1e5ebf0bc92b7ad692a
Jason Swett <jason@benfranklinlabs.com>
2025-06-25 21:36:13 -0400

    Refactoring.

--- app/models/test_run_orchestration_check.rb
+++ app/models/test_run_orchestration_check.rb
@@ -1,10 +1,12 @@
 class TestRunOrchestrationCheck
   TEST_RUNNER_OLDNESS_THRESHOLD = 1.hour
-  attr_accessor :available_test_runners, :unassigned_test_runners
+  attr_reader :available_test_runners, :unassigned_test_runners
 
   def initialize
-    @available_test_runners = nil
-    @unassigned_test_runners = nil
+    ActiveRecord::Base.uncached do
+      @available_test_runners = TestRunner.available.to_a
+      @unassigned_test_runners = TestRunner.unassigned
+    end
   end
 
   def old_test_runners
