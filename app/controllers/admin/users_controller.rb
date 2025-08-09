388236d3bd7148b3255766f35bd0022a5c425fb2
Jason Swett <jason@benfranklinlabs.com>
2025-08-09 13:31:40 -0400

    Bring back old version of user index.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,7 @@
 module Admin
   class UsersController < ApplicationController
     def index
-      @users = User.left_joins(github_accounts: { projects: :test_suite_runs })
-                   .select("users.*, MAX(test_suite_runs.created_at) as most_recent_test_run")
-                   .group("users.id")
-                   .order(Arel.sql("COALESCE(MAX(test_suite_runs.created_at), users.created_at) DESC"))
+      @users = User.order("created_at desc")
       authorize :admin, :index?
     end
   end
