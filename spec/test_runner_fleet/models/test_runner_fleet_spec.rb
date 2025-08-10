b5d0e773fb7354c8dd7e433ddb7516357085ed5e
Jason Swett <jason@benfranklinlabs.com>
2025-08-10 12:23:04 -0400

    Move target size logic from orchestration check to fleet with delegation.

--- spec/test_runner_fleet/models/test_runner_fleet_spec.rb
+++ spec/test_runner_fleet/models/test_runner_fleet_spec.rb
@@ -48,4 +48,25 @@ describe TestRunnerFleet do
       end
     end
   end
+
+  describe ".target_size" do
+    before do
+      stub_const("ENV", ENV.to_hash.merge("TEST_RUNNER_FLEET_SIZE" => "10"))
+      create(:run)
+    end
+
+    context "a run has been created within the last hour" do
+      it "returns TEST_RUNNER_FLEET_SIZE" do
+        expect(TestRunnerFleet.target_size).to eq(10)
+      end
+    end
+
+    context "a run has not been created within the last hour" do
+      it "returns 0" do
+        travel_to(2.hours.from_now) do
+          expect(TestRunnerFleet.target_size).to eq(0)
+        end
+      end
+    end
+  end
 end
