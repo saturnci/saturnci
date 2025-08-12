46cb7acb7e724a3c26b3f515e982d5873c966446
Jason Swett <jason@benfranklinlabs.com>
2025-08-11 22:04:38 -0400

    Add status to email body.

--- app/mailers/test_suite_run_mailer.rb
+++ app/mailers/test_suite_run_mailer.rb
@@ -6,7 +6,7 @@ class TestSuiteRunMailer < ApplicationMailer
 
     mail(
       to: @user.email,
-      subject: "#{@test_suite_run.status} test suite run for #{@repository.name}"
+      subject: "#{@test_suite_run.status}: test suite run for #{@repository.name}"
     )
   end
 end
