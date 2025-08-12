c27aa00f28bf5aef5455879500f8b30230a19233
Jason Swett <jason@benfranklinlabs.com>
2025-08-12 08:53:39 -0400

    Fix test.

--- spec/mailers/test_suite_run_mailer_spec.rb
+++ spec/mailers/test_suite_run_mailer_spec.rb
@@ -15,7 +15,7 @@ describe TestSuiteRunMailer, type: :mailer do
     it "has a subject which includes status, commit message, and repository name" do
       allow(test_suite_run).to receive(:status).and_return("Passed")
       mail = TestSuiteRunMailer.completion_notification(test_suite_run)
-      expect(mail.subject).to eq("Passed: \"Enhance test...\" (2a5b5d81) on acmecorp/billing-system")
+      expect(mail.subject).to eq("Passed: \"Enhance test...\" (2a5b5d8) on acmecorp/billing-system")
     end
   end
 end
