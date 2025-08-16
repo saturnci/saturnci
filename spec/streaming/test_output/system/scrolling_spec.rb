41e88d2104efbcb987c913cbe2f8772322e414aa
Jason Swett <jason@benfranklinlabs.com>
2025-08-16 16:33:41 -0400

    Add failing test for auto-scroll pause when user scrolls up
    The test correctly demonstrates the current problematic behavior where
    auto-scroll continues even after user manually scrolls up in logs.

--- spec/streaming/test_output/system/scrolling_spec.rb
+++ spec/streaming/test_output/system/scrolling_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 describe "Test output scrolling", type: :system do
+  include APIAuthenticationHelper
+  include SaturnAPIHelper
+
   let!(:run) do
     create(:run, test_output: ("line\n" * 500) + "bottom line")
   end
@@ -14,5 +17,28 @@ describe "Test output scrolling", type: :system do
     sleep (TerminalOutputComponent::DOM_RENDER_DELAY_IN_MILLISECONDS + 100) / 1000.0  # Allow DOM render delay + buffer
     log_console = PageObjects::LogConsole.new(page)
     expect(log_console).to have_visible_text("bottom line")
+  end
+
+  it "stops auto-scrolling when user scrolls up" do
+    sleep (TerminalOutputComponent::DOM_RENDER_DELAY_IN_MILLISECONDS + 100) / 1000.0
+    log_console = PageObjects::LogConsole.new(page)
+    
+    expect(log_console).to have_visible_text("bottom line")
+    
+    # Scroll up manually
+    page.execute_script("document.querySelector('.run-details').scrollTop = 0;")
+    sleep 0.1
+    
+    # Trigger new log output via API which should cause auto-scroll
+    new_content = "\nnew top line\n" + ("middle line\n" * 100) + "new bottom line"
+    
+    http_request(
+      api_authorization_headers: api_authorization_headers(run.build.project.user),
+      path: api_v1_run_test_output_path(run_id: run.id, format: :json),
+      body: Base64.encode64(new_content)
+    )
+    
+    expect(page).to have_content("new top line")
+    expect(page).not_to have_content("new bottom line")
   end
 end
