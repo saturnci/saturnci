b7d9a91b3e98484235f3c31e7a85834224896fe9
Jason Swett <jason@benfranklinlabs.com>
2025-08-17 09:14:57 -0400

    Replace setTimeout with double requestAnimationFrame for reliable scrolling.

--- app/javascript/controllers/terminal_output_controller.js
+++ app/javascript/controllers/terminal_output_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { domRenderDelay: Number }
-
   connect() {
     this.autoScrollToBottom()
     this.unmaskTerminal()
@@ -11,9 +9,11 @@ export default class extends Controller {
   autoScrollToBottom() {
     const element = document.querySelector('.run-details')
     
-    setTimeout(() => {
-      element.scrollTop = element.scrollHeight
-    }, this.domRenderDelayValue)
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        element.scrollTop = element.scrollHeight
+      })
+    })
   }
 
   unmaskTerminal() {
