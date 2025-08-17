153cc19330f1dd4fb329b30e7151e1e5acfc7451
Jason Swett <jason@benfranklinlabs.com>
2025-08-17 16:50:39 -0400

    Fix test.

--- spec/streaming/test_output/system/scrolling_spec.rb
+++ spec/streaming/test_output/system/scrolling_spec.rb
@@ -4,37 +4,37 @@ describe "Test output scrolling", type: :system do
   include APIAuthenticationHelper
   include SaturnAPIHelper
   
-  let!(:run) do
-    create(:run, test_output: ("line\n" * 500) + "bottom line")
-  end
-
   before do
     login_as(run.build.project.user, scope: :user)
     visit run_path(run, "test_output")
     sleep 0.3 # Allow DOM to render
   end
 
-  it "scrolls to the bottom" do
-    log_console = PageObjects::LogConsole.new(page)
-    expect(log_console).to have_visible_text("bottom line")
+  context "no scrolling" do
+    let!(:run) do
+      create(:run, test_output: ("line\n" * 500) + "bottom line")
+    end
+
+    it "scrolls to the bottom" do
+      log_console = PageObjects::LogConsole.new(page)
+      expect(log_console).to have_visible_text("bottom line")
+    end
   end
 
   context "when user scrolls up" do
+    let!(:run) do
+      create(:run, test_output: ("line\n" * 500) + "bottom line")
+    end
+
     it "pauses auto-scroll" do
       log_console = PageObjects::LogConsole.new(page)
       expect(log_console).to have_visible_text("bottom line")
 
-      # User scrolls up manually
       page.execute_script("""
         const runDetails = document.querySelector('.run-details');
-        runDetails.scrollTop = 0;
-        runDetails.dispatchEvent(new Event('scroll'));
+        runDetails.scrollTop -= 50;
       """)
-      
-      # Give time for scroll event to be processed
-      sleep 0.1
 
-      # New content arrives via API
       new_content = "\ntop of new content\n" + ("middle line\n" * 100) + "bottom of new content"
       http_request(
         api_authorization_headers: api_authorization_headers(run.build.project.user),
@@ -42,8 +42,10 @@ describe "Test output scrolling", type: :system do
         body: Base64.encode64(new_content)
       )
 
-      expect(page).to have_content("top of new content")
-      expect(page).not_to have_content("bottom of new content")
+      expect(log_console).not_to have_visible_text("bottom of new content")
+
+      page.execute_script("document.querySelector('.run-details').scrollTop = document.querySelector('.run-details').scrollHeight")
+      expect(log_console).to have_visible_text("bottom of new content")
     end
   end
 end
