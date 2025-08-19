59cfdf9b84998956517c61ab2b606c3a225884ec
Jason Swett <jason@benfranklinlabs.com>
2025-08-19 15:40:39 -0400

    Fix permissions_spec.

--- spec/repositories/request/permissions_spec.rb
+++ spec/repositories/request/permissions_spec.rb
@@ -1,35 +1,35 @@
 require "rails_helper"
 
-describe "Project permissions", type: :request do
-  let!(:build) { create(:build) }
-  let!(:project) { build.project }
+describe "Repository permissions", type: :request do
+  let!(:test_suite_run) { create(:test_suite_run) }
+  let!(:repository) { test_suite_run.repository }
 
-  describe "GET project" do
+  describe "GET repository" do
     context "not signed in" do
       it "gives a 404" do
-        get project_path(project)
+        get project_path(repository)
         expect(response).to have_http_status(:not_found)
       end
     end
 
-    context "one's own project" do
+    context "one's own repository" do
       before do
-        login_as(project.user, scope: :user)
+        login_as(repository.user)
       end
 
       it "gives a 302" do
-        get project_path(project)
+        get project_path(repository)
         expect(response).to have_http_status(:found)
       end
     end
 
-    context "somebody else's project" do
+    context "somebody else's repository" do
       before do
-        login_as(create(:user), scope: :user)
+        login_as(create(:user))
       end
 
       it "gives a 404" do
-        get project_path(project)
+        get project_path(repository)
         expect(response).to have_http_status(:not_found)
       end
     end
