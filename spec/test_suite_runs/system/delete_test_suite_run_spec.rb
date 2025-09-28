7251deec3c1b95713f017c65952b8949cafe5930
Jason Swett <jason@benfranklinlabs.com>
2025-09-28 08:03:41 -0400

    Rename delete_build_spec to delete_test_suite_run_spec and update to use repository naming.

--- spec/test_suite_runs/system/delete_test_suite_run_spec.rb
+++ spec/test_suite_runs/system/delete_test_suite_run_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
-describe "Delete build", type: :system do
+describe "Delete test suite run", type: :system do
   let!(:run) { create(:run, :with_test_runner) }
 
   before do
     allow_any_instance_of(User).to receive(:can_access_repository?).and_return(true)
-    login_as(run.build.project.user)
+    login_as(run.build.repository.user)
   end
 
   context "runner still exists on Digital Ocean" do
@@ -14,39 +14,39 @@ describe "Delete build", type: :system do
     end
 
     context "branch is only branch" do
-      it "removes the build" do
-        visit project_build_path(id: run.build.id, project_id: run.build.project.id)
+      it "removes the test suite run" do
+        visit repository_test_suite_run_path(run.build.repository, run.build)
         click_on "Delete"
         expect(page).not_to have_content(run.build.commit_hash)
       end
     end
 
-    context "deleted build is not the only build" do
-      let!(:other_build) do
-        create(:build, :with_run, project: run.build.project)
+    context "deleted test suite run is not the only test suite run" do
+      let!(:other_test_suite_run) do
+        create(:build, :with_run, repository: run.build.repository)
       end
 
       before do
-        visit project_build_path(id: run.build.id, project_id: run.build.project.id)
+        visit repository_test_suite_run_path(run.build.repository, run.build)
         click_on "Delete"
       end
 
-      it "removes the deleted build" do
+      it "removes the deleted test suite run" do
         expect(page).not_to have_content(run.build.commit_hash)
       end
 
-      it "does not remove the other build" do
-        expect(page).to have_content(other_build.commit_hash)
+      it "does not remove the other test suite run" do
+        expect(page).to have_content(other_test_suite_run.commit_hash)
       end
     end
 
-    context "build is finished" do
+    context "test suite run is finished" do
       before do
         run.finish!
       end
 
       it "still works" do
-        visit project_build_path(id: run.build.id, project_id: run.build.project.id)
+        visit repository_test_suite_run_path(run.build.repository, run.build)
         click_on "Delete"
         expect(page).not_to have_content(run.build.commit_hash)
         expect(page).to have_content("SaturnCI")
@@ -59,7 +59,7 @@ describe "Delete build", type: :system do
       stub_request(:delete, "https://api.digitalocean.com/v2/droplets/#{run.test_runner.cloud_id}").to_return(status: 404)
     end
 
-    it "removes the build" do
+    it "removes the test suite run" do
       visit project_build_path(id: run.build.id, project_id: run.build.project.id)
       click_on "Delete"
       expect(page).not_to have_content(run.build.commit_hash)
