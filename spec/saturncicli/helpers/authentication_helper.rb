ecfbc65a8b5f1d61cf0858c77c383dbdaca7e2fa
Jason Swett <jason@benfranklinlabs.com>
2025-11-22 19:34:15 -0500

    Remove saturncicli, which has now been moved to a standalone gem.

--- spec/saturncicli/helpers/authentication_helper.rb
+++
@@ -1,6 +0,0 @@
-module AuthenticationHelper
-  def self.stub_authentication_request
-    WebMock.stub_request(:get, "#{SaturnCICLI::Credential::DEFAULT_HOST}/api/v1/test_suite_runs")
-      .to_return(body: "[]", status: 200)
-  end
-end
