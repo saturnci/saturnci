bffc5da2880deaad74aeccd9434e143ce2e38a99
Jason Swett <jason@benfranklinlabs.com>
2025-11-23 20:12:19 -0500

    Add PersonalAccessToken.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_22_224037) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_24_010348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -62,6 +62,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_22_224037) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "value"], name: "index_github_oauth_tokens_on_user_id_and_value", unique: true
     t.index ["user_id"], name: "index_github_oauth_tokens_on_user_id"
+  end
+
+  create_table "personal_access_tokens", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "value"], name: "index_personal_access_tokens_on_user_id_and_value", unique: true
+    t.index ["user_id"], name: "index_personal_access_tokens_on_user_id"
   end
 
   create_table "project_secrets", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -281,6 +290,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_22_224037) do
   add_foreign_key "github_check_runs", "test_suite_runs", column: "build_id"
   add_foreign_key "github_events", "repositories"
   add_foreign_key "github_oauth_tokens", "users"
+  add_foreign_key "personal_access_tokens", "users"
   add_foreign_key "project_secrets", "repositories", column: "project_id"
   add_foreign_key "repositories", "github_accounts"
   add_foreign_key "run_events", "runs"
---
+++ spec/factories/personal_access_tokens.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :personal_access_token do
+    user
+  end
+end
