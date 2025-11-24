0bec783cc25c90be7524a283237ad682d37525db
Jason Swett <jason@benfranklinlabs.com>
2025-11-23 22:41:23 -0500

    Revert "Revert "Support Personal Access Token authentication with legacy fallback.""
    This reverts commit 0527f2d9449b8f4f0938605bc29b32d96d80c77c.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -7,8 +7,16 @@ module API
 
       def authenticate_api_user!
         authenticate_or_request_with_http_basic do |user_id, api_token|
-          @current_user = User.find_by(id: user_id)
-          @current_user && @current_user.api_token == api_token
+          personal_access_token = PersonalAccessToken.find_by(value: api_token)
+
+          if personal_access_token.present?
+            @current_user = personal_access_token.user
+          else
+            # legacy token
+            @current_user = User.find_by(id: user_id, api_token: api_token)
+          end
+
+          @current_user.present?
         end
       end
 
