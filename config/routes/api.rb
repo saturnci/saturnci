cb1728ea76db65e231e6ccb4e6a0c929e390da41
Jason Swett <jason@benfranklinlabs.com>
2025-11-25 09:30:34 -0500

    Remove old test runner agent endpoints.

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,31 +1,17 @@
 namespace :api do
   namespace :v1 do
-    resources :runs, only: %w[index show] do
-      resources :run_finished_events, only: :create
-      resources :system_logs, only: :create
-      resource :test_output, only: :create
-      resource :json_output, only: :create
-      resources :screenshots, only: :create
-      resources :run_events, only: :create
-      resource :runner, only: :destroy
-    end
-
     resources :runs, only: %w[index show update]
     resource :orphaned_runner_collection, only: :destroy
     resource :test_runner_collection, only: :destroy
     resources :test_suite_runs, only: %i[index update create]
 
-    resources :test_runners, only: %i[index show update destroy] do
-      resources :test_runner_events, only: :create
-      resources :test_runner_assignments, only: :index
-    end
+    resources :test_runners, only: %i[index show update destroy]
 
     resources :github_events
-    resources :github_tokens, only: :create
     resources :job_machine_images, only: :update
     resources :debug_messages, only: :create
 
-    # Test Runner Agents namespace - duplicated endpoints for migration
+    # Test Runner Agents namespace
     namespace :test_runner_agents do
       resources :runs, only: [] do
         resources :run_finished_events, only: :create
