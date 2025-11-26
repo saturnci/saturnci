3a077ff6972fd2a157afc0b330eeb0ec534f8ebb
Jason Swett <jason@benfranklinlabs.com>
2025-11-25 22:10:32 -0500

    Add spec/api/v1/test_runner_agents/authentication_spec.rb.

---
+++ spec/api/v1/test_runner_agents/authentication_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Test Runner Agents Authentication", type: :request do
+  describe "GET /api/v1/test_runner_agents/test_runners/:test_runner_id/test_runner_assignments" do
+    let!(:test_runner_assignment) { create(:test_runner_assignment) }
+    let!(:test_runner) { test_runner_assignment.test_runner }
+
+    context "with valid test runner credentials" do
+      it "returns success" do
+        credentials = ActionController::HttpAuthentication::Basic.encode_credentials(
+          test_runner.id,
+          test_runner.access_token.value
+        )
+
+        get(
+          api_v1_test_runner_agents_test_runner_test_runner_assignments_path(
+            test_runner_id: test_runner.id,
+            format: :json
+          ),
+          headers: { "Authorization" => credentials }
+        )
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
