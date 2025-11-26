57c3dcb62d0fa9bd2c1133f43157ab791c3a6519
Jason Swett <jason@benfranklinlabs.com>
2025-11-26 10:34:17 -0500

    Remove legacy authentication from TestRunnerAgentsAPIController.

--- app/controllers/api/v1/test_runner_agents/test_runner_agents_api_controller.rb
+++ app/controllers/api/v1/test_runner_agents/test_runner_agents_api_controller.rb
@@ -3,29 +3,14 @@ module API
     module TestRunnerAgents
       class TestRunnerAgentsAPIController < ActionController::Base
         skip_before_action :verify_authenticity_token
-        before_action :authenticate_api_user!
+        before_action :authenticate_test_runner!
 
-        def authenticate_api_user!
-          authenticate_or_request_with_http_basic do |user_id, api_token|
+        def authenticate_test_runner!
+          authenticate_or_request_with_http_basic do |test_runner_id, api_token|
             access_token = AccessToken.find_by(value: api_token)
-
-            if access_token.present?
-              @current_test_runner = TestRunner.find_by(id: user_id, access_token: access_token)
-            else
-              # legacy token
-              @current_user = User.find_by(id: user_id, api_token: api_token)
-            end
-
-            @current_user.present? || @current_test_runner.present?
+            @current_test_runner = TestRunner.find_by(id: test_runner_id, access_token: access_token) if access_token.present?
+            @current_test_runner.present?
           end
-        end
-
-        def current_user
-          @current_user
-        end
-
-        def user_not_authorized
-          head :not_found
         end
       end
     end
