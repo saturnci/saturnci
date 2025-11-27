c4874666921bd13482c186d5077048dfdca721df
Jason Swett <jason@benfranklinlabs.com>
2025-11-27 13:34:03 -0500

    Use Worker class in associations instead of TestRunner.

--- app/models/test_runner_assignment.rb
+++ app/models/test_runner_assignment.rb
@@ -1,4 +1,4 @@
 class TestRunnerAssignment < ApplicationRecord
-  belongs_to :test_runner, class_name: "TestRunner", inverse_of: :test_runner_assignment
+  belongs_to :test_runner, class_name: "Worker", inverse_of: :test_runner_assignment
   belongs_to :run
 end
