c861b38dfc59954c76aea9b36a437640ce9461cf
Jason Swett <jason@benfranklinlabs.com>
2025-11-27 21:55:06 -0500

    Squashed commit of the following:
    commit 8600577129fa11ddeee971edea98b2bae5ea6971
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Thu Nov 27 21:34:38 2025 -0500
    
        Add WorkerEvent model.

--- app/models/test_runner_event.rb
+++ app/models/test_runner_event.rb
@@ -1,14 +1,2 @@
-class TestRunnerEvent < ApplicationRecord
-  self.table_name = "worker_events"
-  self.inheritance_column = :_type_not_used
-  belongs_to :worker, foreign_key: :test_runner_id
-
-  enum :type, [
-    :provision_request_sent,
-    :ready_signal_received,
-    :assignment_acknowledged,
-    :error,
-    :test_run_finished,
-    :assignment_made,
-  ]
+class TestRunnerEvent < WorkerEvent
 end
