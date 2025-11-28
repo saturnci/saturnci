8600577129fa11ddeee971edea98b2bae5ea6971
Jason Swett <jason@benfranklinlabs.com>
2025-11-27 21:34:38 -0500

    Add WorkerEvent model.

---
+++ app/models/worker_event.rb
@@ -0,0 +1,14 @@
+class WorkerEvent < ApplicationRecord
+  self.table_name = "worker_events"
+  self.inheritance_column = :_type_not_used
+  belongs_to :worker, foreign_key: :test_runner_id
+
+  enum :type, [
+    :provision_request_sent,
+    :ready_signal_received,
+    :assignment_acknowledged,
+    :error,
+    :test_run_finished,
+    :assignment_made,
+  ]
+end
