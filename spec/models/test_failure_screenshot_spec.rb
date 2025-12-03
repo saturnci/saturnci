2412cd581ab3eaac7d89360b17ba0a34c6aa3fef
Jason Swett <jason@benfranklinlabs.com>
2025-12-03 13:01:20 -0500

    Stub AWS presigner in test_failure_screenshot_spec to avoid real HTTP calls.

--- spec/models/test_failure_screenshot_spec.rb
+++ spec/models/test_failure_screenshot_spec.rb
@@ -6,6 +6,14 @@ describe TestFailureScreenshot do
       create(:test_failure_screenshot, path: "screenshots/test_failure.png")
     end
 
+    before do
+      presigner = instance_double(Aws::S3::Presigner)
+      allow(Aws::S3::Presigner).to receive(:new).and_return(presigner)
+      allow(presigner).to receive(:presigned_url).and_return(
+        "https://example.com/screenshots/test_failure.png?signed=true"
+      )
+    end
+
     it "returns a presigned URL" do
       expect(test_failure_screenshot.url).to include("screenshots/test_failure.png")
     end
