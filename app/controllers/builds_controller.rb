eb0d2354c59e96bf455f68e499e27e3e719693c6
Jason Swett <jason@benfranklinlabs.com>
2025-12-05 17:45:04 -0500

    Fix BuildsController.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -64,6 +64,7 @@ class BuildsController < ApplicationController
 
     @test_suite_run_component = TestSuiteRunComponent.new(
       build: @build,
+      test_suite_run: @build,
       current_tab_name: params[:partial],
       branch_name: params[:branch_name],
       statuses: params[:statuses],
