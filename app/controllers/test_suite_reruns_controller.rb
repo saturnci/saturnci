ee22734a70a7953bf806b760274abf360b6922ba
Jason Swett <jason@benfranklinlabs.com>
2025-12-07 11:41:02 -0500

    Track started_by_user when a user clicks rerun.

--- app/controllers/test_suite_reruns_controller.rb
+++ app/controllers/test_suite_reruns_controller.rb
@@ -1,7 +1,12 @@
 class TestSuiteRerunsController < ApplicationController
   def create
     original_test_suite_run = TestSuiteRun.find(params[:test_suite_run_id])
-    test_suite_run = TestSuiteRerun.create!(original_test_suite_run)
+
+    test_suite_run = TestSuiteRerun.create!(
+      original_test_suite_run,
+      started_by_user: current_user
+    )
+
     authorize test_suite_run, :create?
     test_suite_run.start!
 
--- spec/test_suite_runs/request/test_suite_reruns_spec.rb
+++ spec/test_suite_runs/request/test_suite_reruns_spec.rb
@@ -31,5 +31,14 @@ describe "test suite reruns", type: :request do
       post(test_suite_reruns_path(test_suite_run_id: test_suite_run.id))
       expect(response).to have_http_status(302)
     end
+
+    it "sets started_by_user to the user who made the request" do
+      post(test_suite_reruns_path(test_suite_run_id: test_suite_run.id))
+      new_test_suite_run = TestSuiteRun.where.not(id: test_suite_run.id).find_by!(
+        repository: test_suite_run.repository,
+        commit_hash: test_suite_run.commit_hash
+      )
+      expect(new_test_suite_run.started_by_user).to eq(test_suite_run.project.user)
+    end
   end
 end
