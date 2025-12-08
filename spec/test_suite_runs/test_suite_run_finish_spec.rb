e9f682bd81203906f7afefa732b634bd015688c6
Jason Swett <jason@benfranklinlabs.com>
2025-12-07 23:05:49 -0500

    Add automatic failure rerun when test suite run finishes with failures.

---
+++ spec/test_suite_runs/test_suite_run_finish_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe TestSuiteRunFinish do
+  describe "#process" do
+    context "when no test case runs failed" do
+      let!(:test_suite_run) { create(:test_suite_run) }
+      let!(:run) { create(:run, test_suite_run:) }
+      let!(:test_case_run) { create(:test_case_run, run:, status: "passed") }
+
+      before do
+        allow(test_suite_run).to receive(:check_test_case_run_integrity!)
+      end
+
+      it "does not create a new test suite run" do
+        expect { TestSuiteRunFinish.new(test_suite_run).process }
+          .not_to change { TestSuiteRun.count }
+      end
+    end
+
+    context "when test case runs failed" do
+      let!(:test_suite_run) { create(:test_suite_run) }
+      let!(:run) { create(:run, test_suite_run:) }
+      let!(:failed_test_case_run) { create(:test_case_run, run:, status: "failed") }
+
+      before do
+        allow(test_suite_run).to receive(:check_test_case_run_integrity!)
+      end
+
+      it "creates a new test suite run" do
+        expect { TestSuiteRunFinish.new(test_suite_run).process }
+          .to change { TestSuiteRun.count }.by(1)
+      end
+    end
+  end
+end
