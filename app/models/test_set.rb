d983de275be784835ca97ea519ab15de43ca884c
Jason Swett <jason@benfranklinlabs.com>
2025-12-08 20:44:26 -0500

    Simplify TestSet#grouped using ActiveSupport in_groups.

--- app/models/test_set.rb
+++ app/models/test_set.rb
@@ -4,19 +4,8 @@ class TestSet
   end
 
   def grouped(number_of_groups)
-    base_size = @test_files.size / number_of_groups
-    remainder = @test_files.size % number_of_groups
-
-    result = {}
-    start_index = 0
-
-    number_of_groups.times do |i|
-      chunk_size = base_size + (i < remainder ? 1 : 0)
-      group_index = i + 1
-      result[group_index.to_s] = @test_files[start_index, chunk_size]
-      start_index += chunk_size
-    end
-
-    result
+    @test_files.in_groups(number_of_groups, false)
+      .each_with_index
+      .to_h { |group, index| [(index + 1).to_s, group] }
   end
 end
