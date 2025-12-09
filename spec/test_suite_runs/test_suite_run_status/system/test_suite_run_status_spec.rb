1d3823600a59a7d7fb6732be3e8d26672feb4195
Jason Swett <jason@benfranklinlabs.com>
2025-12-09 18:07:19 -0500

    Change "run" to "task" in test.

--- spec/test_suite_runs/test_suite_run_status/system/test_suite_run_status_spec.rb
+++ spec/test_suite_runs/test_suite_run_status/system/test_suite_run_status_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 describe "Test suite run status", type: :system do
   include SaturnAPIHelper
 
-  let!(:run) { create(:run, :with_worker) }
-  let!(:user) { run.test_suite_run.repository.user }
+  let!(:task) { create(:task, :with_worker) }
+  let!(:user) { task.test_suite_run.repository.user }
 
   before do
     allow(Nova).to receive(:delete_k8s_job)
@@ -16,15 +16,15 @@ describe "Test suite run status", type: :system do
   context "test suite run goes from running to passed" do
     context "no page refresh" do
       it "changes the status from running to passed" do
-        visit repository_build_path(id: run.test_suite_run.id, repository_id: run.test_suite_run.repository.id)
+        visit repository_build_path(id: task.test_suite_run.id, repository_id: task.test_suite_run.repository.id)
         expect(page).to have_content("Running")
 
-        run.update!(json_output: { "summary" => { "failure_count" => 0 } }.to_json)
+        task.update!(json_output: { "summary" => { "failure_count" => 0 } }.to_json)
 
         perform_enqueued_jobs do
           http_request(
-            api_authorization_headers: worker_agents_api_authorization_headers(run.worker),
-            path: api_v1_worker_agents_task_task_finished_events_path(task_id: run.id)
+            api_authorization_headers: worker_agents_api_authorization_headers(task.worker),
+            path: api_v1_worker_agents_task_task_finished_events_path(task_id: task.id)
           )
         end
 
@@ -34,21 +34,21 @@ describe "Test suite run status", type: :system do
 
     context "full page refresh after test suite run finishes" do
       it "maintains the 'passed' status" do
-        visit repository_build_path(id: run.test_suite_run.id, repository_id: run.test_suite_run.repository.id)
+        visit repository_build_path(id: task.test_suite_run.id, repository_id: task.test_suite_run.repository.id)
         expect(page).to have_content("Running")
 
-        run.update!(json_output: { "summary" => { "failure_count" => 0 } }.to_json)
+        task.update!(json_output: { "summary" => { "failure_count" => 0 } }.to_json)
 
         perform_enqueued_jobs do
           http_request(
-            api_authorization_headers: worker_agents_api_authorization_headers(run.worker),
-            path: api_v1_worker_agents_task_task_finished_events_path(task_id: run.id)
+            api_authorization_headers: worker_agents_api_authorization_headers(task.worker),
+            path: api_v1_worker_agents_task_task_finished_events_path(task_id: task.id)
           )
         end
 
         expect(page).to have_content("Passed") # to prevent race condition
 
-        visit repository_build_path(id: run.test_suite_run.id, repository_id: run.test_suite_run.repository.id)
+        visit repository_build_path(id: task.test_suite_run.id, repository_id: task.test_suite_run.repository.id)
         expect(page).to have_content("Passed")
       end
     end
@@ -56,11 +56,11 @@ describe "Test suite run status", type: :system do
 
   describe "test suite run list links" do
     context "test suite run goes from running to finished" do
-      let!(:other_test_suite_run) { create(:build, repository: run.test_suite_run.repository) }
-      let!(:other_run) { create(:run, :with_worker, build: other_test_suite_run) }
+      let!(:other_test_suite_run) { create(:build, repository: task.test_suite_run.repository) }
+      let!(:other_task) { create(:run, :with_worker, build: other_test_suite_run) }
 
       it "maintains the currently active build" do
-        visit repository_build_path(id: run.test_suite_run.id, repository_id: run.test_suite_run.repository.id)
+        visit repository_build_path(id: task.test_suite_run.id, repository_id: task.test_suite_run.repository.id)
 
         within ".test-suite-run-list" do
           expect(page).to have_content("Running", count: 2)
@@ -68,13 +68,13 @@ describe "Test suite run status", type: :system do
 
         perform_enqueued_jobs do
           http_request(
-            api_authorization_headers: worker_agents_api_authorization_headers(other_run.worker),
-            path: api_v1_worker_agents_task_task_finished_events_path(task_id: other_run.id)
+            api_authorization_headers: worker_agents_api_authorization_headers(other_task.worker),
+            path: api_v1_worker_agents_task_task_finished_events_path(task_id: other_task.id)
           )
         end
 
-        other_run_test_suite_run_link = PageObjects::TestSuiteRunLink.new(page, other_test_suite_run)
-        expect(other_run_test_suite_run_link).not_to be_active
+        other_task_test_suite_run_link = PageObjects::TestSuiteRunLink.new(page, other_test_suite_run)
+        expect(other_task_test_suite_run_link).not_to be_active
       end
     end
   end
@@ -82,15 +82,15 @@ describe "Test suite run status", type: :system do
   describe "elapsed time" do
     context "running test suite run" do
       it "does not show the elapsed test suite run time" do
-        visit repository_build_path(run.test_suite_run.repository, run.test_suite_run)
+        visit repository_build_path(task.test_suite_run.repository, task.test_suite_run)
         expect(page).to have_selector("[data-elapsed-test-suite-run-time-target='value']")
       end
     end
 
     context "finished test suite run" do
       it "shows the elapsed test suite run time" do
-        run.update!(exit_code: 0)
-        visit repository_build_path(run.test_suite_run.repository, run.test_suite_run)
+        task.update!(exit_code: 0)
+        visit repository_build_path(task.test_suite_run.repository, task.test_suite_run)
         expect(page).not_to have_selector("[data-elapsed-test-suite-run-time-target='value']")
       end
     end
@@ -98,7 +98,7 @@ describe "Test suite run status", type: :system do
     context "when the test suite run goes from running to finished" do
       before do
         allow_any_instance_of(TestSuiteRun).to receive(:duration_formatted).and_return("3m 40s")
-        visit repository_build_path(run.test_suite_run.repository, run.test_suite_run)
+        visit repository_build_path(task.test_suite_run.repository, task.test_suite_run)
       end
 
       it "changes the elapsed time from counting to not counting" do
@@ -108,8 +108,8 @@ describe "Test suite run status", type: :system do
 
         perform_enqueued_jobs do
           http_request(
-            api_authorization_headers: worker_agents_api_authorization_headers(run.worker),
-            path: api_v1_worker_agents_task_task_finished_events_path(task_id: run.id)
+            api_authorization_headers: worker_agents_api_authorization_headers(task.worker),
+            path: api_v1_worker_agents_task_task_finished_events_path(task_id: task.id)
           )
         end
 
