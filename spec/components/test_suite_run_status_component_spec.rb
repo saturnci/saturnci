0441d6f1bc936dff937d26919b5d94d835d39805
Jason Swett <jason@benfranklinlabs.com>
2025-12-09 21:59:55 -0500

    Show failed test case count next to status in sidebar.

--- spec/components/test_suite_run_status_component_spec.rb
+++ spec/components/test_suite_run_status_component_spec.rb
@@ -5,17 +5,32 @@ describe TestSuiteRunStatusComponent, type: :component do
     let!(:task) { create(:task, :passed) }
 
     before do
-      create_list(:test_case_run, 3, task:, status: :passed)
+      create_list(:test_case_run, 7, task:, status: :passed)
     end
 
     it "shows the passed count next to the status" do
       component = TestSuiteRunStatusComponent.new(test_suite_run: task.test_suite_run)
       expect(render_inline(component).text).to include("Passed")
-      expect(render_inline(component).text).to include("(3)")
+      expect(render_inline(component).text).to include("(7)")
     end
   end
 
-  context "test suite run has not passed" do
+  context "test suite run has failed" do
+    let!(:task) { create(:task, :failed) }
+
+    before do
+      create_list(:test_case_run, 2, task:, status: :failed)
+      create_list(:test_case_run, 5, task:, status: :passed)
+    end
+
+    it "shows the failed count and total next to the status" do
+      component = TestSuiteRunStatusComponent.new(test_suite_run: task.test_suite_run)
+      expect(render_inline(component).text).to include("Failed")
+      expect(render_inline(component).text).to include("(2/7)")
+    end
+  end
+
+  context "test suite run is running" do
     let!(:test_suite_run) { create(:test_suite_run, :with_task) }
 
     it "shows just the status without a count" do
