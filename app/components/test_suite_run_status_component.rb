d9925454f51c0c4d6d2b7a0122780a45c865b65c
Jason Swett <jason@benfranklinlabs.com>
2025-12-09 22:03:26 -0500

    Extract count methods from template to component.

--- app/components/test_suite_run_status_component.rb
+++ app/components/test_suite_run_status_component.rb
@@ -4,4 +4,18 @@ class TestSuiteRunStatusComponent < ViewComponent::Base
   def initialize(test_suite_run:)
     @test_suite_run = test_suite_run
   end
+
+  private
+
+  def passed_count
+    test_suite_run.test_case_runs.passed.count
+  end
+
+  def failed_count
+    test_suite_run.test_case_runs.failed.count
+  end
+
+  def total_count
+    test_suite_run.test_case_runs.count
+  end
 end
