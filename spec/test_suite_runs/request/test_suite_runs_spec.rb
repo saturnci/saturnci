85f262b889c953331e0e8e2a5c56644b6959705b
Jason Swett <jason@benfranklinlabs.com>
2025-12-10 22:48:10 -0500

    Update variable names.

--- spec/test_suite_runs/request/test_suite_runs_spec.rb
+++ spec/test_suite_runs/request/test_suite_runs_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 describe "test suite runs", type: :request do
-  let!(:test_suite_run) { create(:build) }
-  before { login_as(test_suite_run.project.user) }
+  let!(:test_suite_run) { create(:test_suite_run) }
+  before { login_as(test_suite_run.repository.user) }
 
   describe "GET test_suite_runs" do
     it "include test suite run" do
       get(
-        project_test_suite_runs_path(project_id: test_suite_run.project_id),
+        repository_test_suite_runs_path(repository_id: test_suite_run.repository_id),
         headers: { "Accept" => "text/vnd.turbo-stream.html" }
       )
 
