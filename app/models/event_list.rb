df7b34856063f3cdca20f2d0775f5031eb11c07d
Jason Swett <jason@benfranklinlabs.com>
2025-12-13 11:43:59 -0500

    Add EventList class with duration_since_previous method.

---
+++ app/models/event_list.rb
@@ -0,0 +1,11 @@
+class EventList
+  def initialize(events)
+    @events = events
+  end
+
+  def duration_since_previous(index)
+    return nil if index.zero?
+
+    (@events[index].created_at - @events[index - 1].created_at).round(2)
+  end
+end
---
+++ spec/models/event_list_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe EventList do
+  describe "#duration_since_previous" do
+    let!(:event1) { create(:worker_event, created_at: Time.zone.parse("2025-01-01 12:00:00")) }
+    let!(:event2) { create(:worker_event, created_at: Time.zone.parse("2025-01-01 12:00:10")) }
+    let!(:event_list) { EventList.new([event1, event2]) }
+
+    context "when index is 0" do
+      it "returns nil" do
+        expect(event_list.duration_since_previous(0)).to be_nil
+      end
+    end
+
+    context "when index is greater than 0" do
+      it "returns the seconds between the event and the previous event" do
+        expect(event_list.duration_since_previous(1)).to eq(10.0)
+      end
+    end
+  end
+end
